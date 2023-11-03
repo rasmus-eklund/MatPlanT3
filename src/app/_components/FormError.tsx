09551ea3a6e6d8aa165d3205d714998b4873d716
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-03 10:19:44 +0100

    feat: classname for errors

--- src/app/_components/FormError.tsx
+++ src/app/_components/FormError.tsx
@@ -1,9 +1,11 @@
 import type { FieldError } from "react-hook-form";
 
-const FormError = ({ error }: { error: FieldError | undefined }) => {
+type Props = { error: FieldError | undefined; className?: string };
+
+const FormError = ({ error, className }: Props) => {
   if (error?.message)
     return (
-      <p className="rounded-md bg-c1 bg-opacity-30 px-2 text-sm">
+      <p className={`rounded-md bg-c1 bg-opacity-30 px-2 text-sm ${className}`}>
         {error.message}
       </p>
     );
