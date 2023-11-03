46ebe7f3ed77b1fd3d31779b0b8b5130783cb3c2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-03 10:20:08 +0100

    feat: helper for state crud

---
+++ src/app/utils/stateCrud.ts
@@ -0,0 +1,25 @@
+import { Dispatch, SetStateAction } from "react";
+
+const crudFactory = <T extends { id: string }>(
+  fn: Dispatch<SetStateAction<T[]>>,
+) => {
+  const add = (item: T) => {
+    fn((p) => [...p, item]);
+  };
+  const remove = (id: string) => {
+    fn((p) => p.filter((i) => i.id !== id));
+  };
+  const update = (item: T) => {
+    fn((p) => {
+      const index = p.findIndex((i) => i.id === item.id);
+      const newItems = [...p];
+      if (index !== -1) {
+        newItems[index] = item;
+      }
+      return newItems;
+    });
+  };
+  return { add, remove, update };
+};
+
+export default crudFactory;
