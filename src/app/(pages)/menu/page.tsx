bd9b8b3015f1b8c41165f4d7c67dfd91ac0125f5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-04 08:22:42 +0100

    feat: menu page

--- src/app/(pages)/menu/page.tsx
+++ src/app/(pages)/menu/page.tsx
@@ -1,9 +1,24 @@
-import { getServerAuthSession } from "~/server/auth";
+import MenuItemComponent from "~/app/_components/menu/MenuItemComponent";
+import days from "~/app/constants/days";
+import sortByName from "~/app/utils/sortByName";
+import { api } from "~/trpc/server";
 
 const Menu = async () => {
-  const session = await getServerAuthSession();
-  console.log(session);
-  return <p>{`menu, logged in: ${session?.user.name}`}</p>;
+  const menu = await api.menu.getAll.query();
+  return (
+    <ul className="flex flex-col gap-2 p-2">
+      {days.map((day) => (
+        <li key={day} className="flex flex-col rounded-md bg-c3 p-2">
+          <h2 className="rounded-md p-2 text-xl font-bold text-c5">{day}</h2>
+          <ul className="flex flex-col gap-2">
+            {sortByName(menu.filter((i) => i.day === day)).map((item) => (
+              <MenuItemComponent key={item.id} item={item} />
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Menu;
