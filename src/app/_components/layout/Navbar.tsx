55cf04dfeaec5fc0423710914e8032738a9638cb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-04 15:49:48 +0100

    refactor icons

--- src/app/_components/layout/Navbar.tsx
+++ src/app/_components/layout/Navbar.tsx
@@ -1,35 +1,24 @@
-import type { ReactNode } from "react";
-import HomeIcon from "../icons/HomeIcon";
-import RecipeIcon from "../icons/RecipesIcon";
-import ItemsIcon from "../icons/ItemsIcon";
-import ShoppingListIcon from "../icons/ShoppingListIcon";
-import StoreIcon from "../icons/StoreIcon";
 import Link from "next/link";
+import Icon from "../icons/Icon";
+import type { Ticon } from "types";
 
 const Navbar = () => {
-  const className = "fill-c2 h-6 md:h-10 hover:scale-110";
-  const home = <HomeIcon className={className} />;
-  const recipes = <RecipeIcon className={className} />;
-  const itemsIcon = <ItemsIcon className={className} />;
-  const shoppingList = <ShoppingListIcon className={className} />;
-  const store = <StoreIcon className={className} />;
-
-  const items: { name: string; href: string; child: ReactNode }[] = [
-    { name: "Meny", href: "/menu", child: home },
-    { name: "Maträtter", href: "/recipes/search", child: recipes },
-    { name: "Varor", href: "/ingredients", child: itemsIcon },
-    { name: "Inköpslista", href: "/shoppingList", child: shoppingList },
-    { name: "Butik", href: "/stores", child: store },
+  const items: { name: string; href: string; icon: Ticon }[] = [
+    { name: "Meny", href: "/menu", icon: "home" },
+    { name: "Maträtter", href: "/recipes/search", icon: "recipes" },
+    { name: "Varor", href: "/ingredients", icon: "pizza" },
+    { name: "Inköpslista", href: "/shoppingList", icon: "cart" },
+    { name: "Butik", href: "/stores", icon: "store" },
   ];
 
   return (
     <nav className="bg-c5">
       <ul className="flex justify-evenly px-4 py-4">
-        {items.map(({ name, href, child }) => (
+        {items.map(({ name, href, icon }) => (
           <li key={name}>
             <Link className="flex flex-col items-center" href={href}>
-              {child}
-              <h3 className="text-c2 font-bold">{name}</h3>
+              <Icon className="h-10 w-10 md:h-12" icon={icon} />
+              <h3 className="text-sm font-bold text-c2 md:text-base">{name}</h3>
             </Link>
           </li>
         ))}
