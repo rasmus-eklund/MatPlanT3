5657a78b2e7f6e0b86812dce256bf2d2b151febb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:56:05 +0100

    refactor icon style

--- src/app/_components/layout/Navbar.tsx
+++ src/app/_components/layout/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 import Icon from "../icons/Icon";
-import type { Ticon } from "types";
+import type { tIcon } from "types";
 
 const Navbar = () => {
-  const items: { name: string; href: string; icon: Ticon }[] = [
+  const items: { name: string; href: string; icon: tIcon }[] = [
     { name: "Meny", href: "/menu", icon: "home" },
     { name: "Maträtter", href: "/recipes/search", icon: "recipes" },
-    { name: "Varor", href: "/ingredients", icon: "pizza" },
+    { name: "Varor", href: "/items", icon: "pizza" },
     { name: "Inköpslista", href: "/shoppingList", icon: "cart" },
     { name: "Butik", href: "/stores", icon: "store" },
   ];
@@ -17,7 +17,10 @@ const Navbar = () => {
         {items.map(({ name, href, icon }) => (
           <li key={name}>
             <Link className="flex flex-col items-center" href={href}>
-              <Icon className="h-10 w-10 md:h-12" icon={icon} />
+              <Icon
+                className="h-10 w-10 fill-c3 hover:scale-110 md:h-12"
+                icon={icon}
+              />
               <h3 className="text-sm font-bold text-c2 md:text-base">{name}</h3>
             </Link>
           </li>
