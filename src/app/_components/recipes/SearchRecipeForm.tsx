0e7ce30268fefb14b3e5fd2dbbdcbefdfd439488
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:57:06 +0100

    style

--- src/app/_components/recipes/SearchRecipeForm.tsx
+++ src/app/_components/recipes/SearchRecipeForm.tsx
@@ -13,10 +13,10 @@ const SearchRecipeForm = () => {
         e.preventDefault();
         router.replace(`/recipes/search?search=${search}`);
       }}
-      className="relative flex gap-2"
+      className="flex h-10 grow gap-2 rounded-md bg-c1 px-2 text-xl"
     >
       <input
-        className="h-10 min-w-0 rounded-md bg-c2 px-2 text-xl"
+        className="min-w-0 grow bg-c1 focus:outline-none"
         id="search"
         name="search"
         type="text"
@@ -24,8 +24,8 @@ const SearchRecipeForm = () => {
         onChange={(e) => setSearch(e.target.value)}
         placeholder={"Sök"}
       />
-      <button className="absolute right-2 top-2" type="submit">
-        <Icon className="" icon="search" />
+      <button type="submit">
+        <Icon className="h-8 w-8 fill-c3 hover:fill-c5" icon="search" />
       </button>
     </form>
   );
