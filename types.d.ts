726d0ac5e6ae1859d9c4b777e48a112ca8d93abf
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:53:18 +0100

    add types for shopping list

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,7 @@
 import days from "~/app/constants/days";
 import units from "~/app/constants/units";
+import svgPaths from "~/app/_components/icons/svgPaths";
+import { RouterOutputs } from "~/trpc/shared";
 export type CategoryItem = {
   name: string;
   id: number;
@@ -8,10 +10,28 @@ export type CategoryItem = {
 
 export type Day = (typeof days)[number];
 export type Unit = (typeof units)[number];
+export type tIcon = keyof typeof svgPaths;
 
 export type SearchIngredient = {
   ingredientId: string;
   name: string;
   category: string;
   subcategory: string;
+};
+
+type StoreOrder = RouterOutputs["store"]["getAll"][number];
+
+export type tItemFilter = {
+  group: boolean;
+  hideRecipe: boolean;
+  selectedStore: StoreOrder;
+};
+
+type tItem = RouterOutputs["item"]["getAll"][number];
+
+export type tItemsGrouped = {
+  name: string;
+  subcategoryId: number;
+  checked: boolean;
+  group: tItem[];
 };
