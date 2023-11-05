1489898674b19c90e13e737949a3923b208249ac
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:57:16 +0100

    change folder name

--- src/app/_components/recipes/ShowRecipe.tsx
+++ src/app/_components/recipes/ShowRecipe.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { ReactNode } from "react";
-import capitalize from "~/app/utils/capitalize";
+import capitalize from "~/app/helpers/capitalize";
 import { RouterOutputs } from "~/trpc/shared";
 
 type Recipe = RouterOutputs["recipe"]["getById"];
