e093db7d9d9d853ab5d8bdb68566496baaac66c3
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:55:28 +0100

    refactor icon style

--- src/app/_components/icons/Icon.tsx
+++ src/app/_components/icons/Icon.tsx
@@ -1,23 +1,15 @@
 import type { RefObject, SVGProps } from "react";
-import type { Ticon } from "types";
+import type { tIcon } from "types";
 import svgPaths from "./svgPaths";
 
 type Props = SVGProps<SVGElement> & {
   ref?: RefObject<SVGSVGElement>;
-  icon: Ticon;
-  reversed?: boolean;
-  className?: string;
+  icon: tIcon;
 };
 
-const Icon = ({ icon, className, reversed = false, ...rest }: Props) => {
-  const colors = reversed ? "fill-c5 hover:fill-c4" : "fill-c4 hover:fill-c5";
+const Icon = ({ icon, ...rest }: Props) => {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      {...rest}
-      className={`h-6 hover:scale-125 ${colors} ${className}`}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...rest}>
       <path d={svgPaths[icon]} />
     </svg>
   );
