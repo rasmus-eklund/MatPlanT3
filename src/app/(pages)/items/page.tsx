4eaff7054725ef348f0f9ae7223b764b69de2bb0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:53:32 +0100

    feat: shopping list

---
+++ src/app/(pages)/items/page.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import EditIngredient from "~/app/_components/EditIngredient";
+import SearchIngredients from "~/app/_components/SearchIngredient";
+import EditItemHome from "~/app/_components/items/EditItemHome";
+import { api } from "~/trpc/react";
+
+const Items = () => {
+  const {
+    data: items,
+    isSuccess,
+    isLoading,
+    isError,
+    refetch,
+  } = api.item.getAll.useQuery();
+  const { mutate: add } = api.item.add.useMutation({
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const { mutate: remove } = api.item.delete.useMutation({
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const { mutate: edit } = api.item.edit.useMutation({
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const { mutate: addHome } = api.home.add.useMutation({
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const { mutate: removeHome } = api.home.remove.useMutation({
+    onSuccess: () => {
+      refetch();
+    },
+  });
+  const handleHome = (isHome: boolean, ingredientId: string) => {
+    isHome ? removeHome({ ingredientId }) : addHome({ ingredientId });
+  };
+
+  return (
+    <div className="flex flex-col gap-2 p-2">
+      <SearchIngredients
+        onSubmit={({ ingredientId, name }) => add({ name, ingredientId })}
+      />
+      <div className="rounded-md bg-c3 p-3">
+        <h2 className="text-c5">Extra varor:</h2>
+        <ul className="flex flex-col gap-2">
+          {isSuccess &&
+            items
+              .filter((i) => !!!i.recipe)
+              .map((item) => (
+                <EditIngredient
+                  key={item.id}
+                  ingredient={item}
+                  onEdit={edit}
+                  onRemove={remove}
+                />
+              ))}
+        </ul>
+      </div>
+      <div className="rounded-md bg-c3 p-3">
+        <h2 className="text-c5">Recept varor:</h2>
+        <ul className="flex flex-col gap-2">
+          {isSuccess &&
+            items
+              .filter((i) => i.recipe)
+              .map((item) => (
+                <EditItemHome
+                  key={item.id}
+                  ingredient={item}
+                  onHome={(home) => handleHome(home, item.ingredientId)}
+                />
+              ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Items;
