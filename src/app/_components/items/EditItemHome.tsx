c0e18a02d13bb432a8c144e54e07f7ae25317a74
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-05 12:55:57 +0100

    feat: ingredient home toggle

---
+++ src/app/_components/items/EditItemHome.tsx
@@ -0,0 +1,33 @@
+"use client";
+import capitalize from "~/app/helpers/capitalize";
+import { RouterOutputs } from "~/trpc/shared";
+import Icon from "../icons/Icon";
+import { api } from "~/trpc/react";
+type Item = RouterOutputs["item"]["getAll"][number];
+type Props = {
+  ingredient: Item;
+  onHome: (home: boolean) => void;
+};
+
+const EditItemHome = ({
+  ingredient: { name, quantity, unit, ingredientId, home },
+  onHome,
+}: Props) => {
+  return (
+    <li className="flex items-center justify-between rounded-md bg-c2 p-1 text-sm text-c4">
+      <p className="grow">{capitalize(name)}</p>
+      <div className="flex items-center gap-2 justify-self-end">
+        <p> {`${quantity} ${unit}`}</p>
+        <Icon
+          icon="home"
+          className={`h-6 w-6 rounded-md bg-c3 ${
+            home ? "fill-c5 hover:fill-c2" : "fill-c2 hover:fill-c5"
+          }`}
+          onClick={() => onHome(home)}
+        />
+      </div>
+    </li>
+  );
+};
+
+export default EditItemHome;
