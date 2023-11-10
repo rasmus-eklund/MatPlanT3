d8a6ddcc85a81ff3e307c562d527ef8becf8c7f1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 09:46:57 +0100

    change layout

--- src/app/_components/layout/Navbar.tsx
+++ src/app/_components/layout/Navbar.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import Icon from "../icons/Icon";
 import type { tIcon } from "types";
+import { UserRole } from "@prisma/client";
 
-const Navbar = () => {
+type Props = { role: UserRole };
+const Navbar = async ({ role }: Props) => {
   const items: { name: string; href: string; icon: tIcon }[] = [
     { name: "Meny", href: "/menu", icon: "home" },
     { name: "Maträtter", href: "/recipes/search", icon: "recipes" },
@@ -12,7 +14,7 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="bg-c5">
+    <nav className="bg-c5 grow">
       <ul className="flex justify-evenly px-4 py-4">
         {items.map(({ name, href, icon }) => (
           <li key={name}>
@@ -25,6 +27,17 @@ const Navbar = () => {
             </Link>
           </li>
         ))}
+        {role === "ADMIN" && (
+          <li>
+            <Link className="flex flex-col items-center" href={"/admin"}>
+              <Icon
+                className="h-10 w-10 fill-c3 hover:scale-110 md:h-12"
+                icon="admin"
+              />
+              <h3 className="text-sm font-bold text-c2 md:text-base">Admin</h3>
+            </Link>
+          </li>
+        )}
       </ul>
     </nav>
   );
