06b5c14eb4fe558f33b51bb6ce975d7da0f0da63
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 09:45:28 +0100

    feat: admin page

---
+++ src/app/(pages)/admin/page.tsx
@@ -0,0 +1,17 @@
+import { redirect } from "next/navigation";
+import ShowIngredients from "~/app/_components/admin/ShowIngredients";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+
+const Admin = async () => {
+  const session = await getServerAuthSession();
+  if (session && session.user.role !== "ADMIN") {
+    redirect("/");
+  }
+
+  const ingredients = await api.admin.getAll.query();
+  const allCats = await api.admin.categories.query();
+  return <ShowIngredients ingredients={ingredients} allCats={allCats} />;
+};
+
+export default Admin;
