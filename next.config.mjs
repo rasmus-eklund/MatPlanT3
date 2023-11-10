5739a91f397f0d4710a46f34eefc6f65059bef67
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 09:43:53 +0100

    feat: next 14

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,6 @@ await import("./src/env.mjs");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
