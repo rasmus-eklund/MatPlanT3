4df5517b62e578dd061ae2eee99c0c644424df38
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-10 09:45:19 +0100

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import { getServerAuthSession } from "../server/auth";
 import LoginHome from "./_components/layout/LoginHome";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 const Home = async () => {
   const session = await getServerAuthSession();
-
+  if (session) {
+    redirect("/menu");
+  }
   return (
-    <div className="flex justify-center items-center h-full">
+    <div className="flex h-full items-center justify-center">
       <Image
         className="z-0 h-full w-full"
         src="/bgimage_sm.jpg"
@@ -18,11 +21,11 @@ const Home = async () => {
         objectFit="cover"
         objectPosition="center"
       />
-      <div className="bg-c1/50 z-10 flex flex-col items-center gap-5 rounded-3xl p-10">
-        <h1 className="text-c5 z-10 text-center text-2xl font-bold">
+      <div className="z-10 flex flex-col items-center gap-5 rounded-3xl bg-c1/50 p-10">
+        <h1 className="z-10 text-center text-2xl font-bold text-c5">
           Välkommen till MatPlan!
         </h1>
-        <p className="text-c5 text-center text-xl font-bold">
+        <p className="text-center text-xl font-bold text-c5">
           Planera måltider, förenkla inköpslistor och effektivisera dina
           matinköp.
         </p>
