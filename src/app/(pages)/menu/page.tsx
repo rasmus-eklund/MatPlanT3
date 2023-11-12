06b22897635e2a7a6e337b5d3023a40151550dde
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-12 07:39:56 +0100

    Refactor: menu items

--- src/app/(pages)/menu/page.tsx
+++ src/app/(pages)/menu/page.tsx
@@ -1,19 +1,32 @@
+"use client";
+import LoadingSpinner from "~/app/_components/LoadingSpinner";
 import MenuItemComponent from "~/app/_components/menu/MenuItemComponent";
 import days from "~/app/constants/days";
 import sortByName from "~/app/helpers/sortByName";
-import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
 
-const Menu = async () => {
-  const menu = await api.menu.getAll.query();
+const Menu = () => {
+  const {
+    data: menu,
+    refetch,
+    isSuccess,
+    isLoading,
+  } = api.menu.getAll.useQuery();
   return (
     <ul className="flex flex-col gap-2 p-2">
       {days.map((day) => (
         <li key={day} className="flex flex-col rounded-md bg-c3 p-2">
           <h2 className="rounded-md p-2 text-xl font-bold text-c5">{day}</h2>
           <ul className="flex flex-col gap-2">
-            {sortByName(menu.filter((i) => i.day === day)).map((item) => (
-              <MenuItemComponent key={item.id} item={item} />
-            ))}
+            {isSuccess &&
+              sortByName(menu.filter((i) => i.day === day)).map((item) => (
+                <MenuItemComponent
+                  key={item.id}
+                  item={item}
+                  update={() => refetch()}
+                />
+              ))}
+            {isLoading && <LoadingSpinner />}
           </ul>
         </li>
       ))}
--- src/app/_components/menu/MenuItemComponent.tsx
+++ src/app/_components/menu/MenuItemComponent.tsx
@@ -1,54 +1,91 @@
 "use client";
 import Link from "next/link";
-import Incrementer from "./Incrementer";
 import { RouterOutputs } from "~/trpc/shared";
 import Icon from "../icons/Icon";
 import { api } from "~/trpc/react";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import LoadingSpinner from "../LoadingSpinner";
 
 type MenuItem = RouterOutputs["menu"]["getAll"][number];
 type Props = {
   item: MenuItem;
+  update: () => void;
 };
 
-const MenuItem = ({ item: { id, name, portions } }: Props) => {
-  const router = useRouter();
+const MenuItem = ({ item, update }: Props) => {
+  const { id, name, portions } = item;
   const { mutate: remove, isLoading: removing } = api.menu.remove.useMutation({
-    onSuccess: () => {
-      router.refresh();
-    },
+    onSuccess: update,
   });
-  const { mutate: changePortions, isLoading: changingPortions } =
-    api.menu.changePortions.useMutation();
-  const [port, setPort] = useState(portions);
   return (
     <li className="flex flex-col gap-2 rounded-md bg-c2 px-2 font-bold text-c5">
-      <Link
-        href={`/menu/${id}`}
-        className="w-full"
-      >
+      <Link href={`/menu/${id}`} className="w-full">
         {name}
       </Link>
       <div className="flex w-full items-center justify-between gap-1">
-        <Incrementer
-          value={port}
-          disabled={changingPortions}
-          callback={(value) => {
-            if (value !== port) {
-              setPort(value);
-              changePortions({ id, portions: value });
-            }
-          }}
-        />
+        <div className="flex select-none items-center gap-1 p-2">
+          <Minus id={id} portions={portions} update={update} />
+          <p className="text-lg">{portions}</p>
+          <Plus id={id} portions={portions} update={update} />
+        </div>
         <button disabled={removing} onClick={() => remove({ id })}>
-          <Icon
-            className="h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5"
-            icon="delete"
-          />
+          {removing ? (
+            <LoadingSpinner className="h-5" />
+          ) : (
+            <Icon
+              className="h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5"
+              icon="delete"
+            />
+          )}
         </button>
       </div>
     </li>
+  );
+};
+
+type ButtonProps = { id: string; portions: number; update: () => void };
+const Plus = ({ id, portions, update }: ButtonProps) => {
+  const { mutate, isLoading } = api.menu.changePortions.useMutation({
+    onSuccess: update,
+  });
+  return (
+    <button
+      disabled={isLoading}
+      onClick={() => mutate({ id, portions: portions + 1 })}
+    >
+      {isLoading ? (
+        <LoadingSpinner className="h-5" />
+      ) : (
+        <Icon
+          className="h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5"
+          icon="plus"
+        />
+      )}
+    </button>
+  );
+};
+const Minus = ({ id, portions, update }: ButtonProps) => {
+  const { mutate, isLoading } = api.menu.changePortions.useMutation({
+    onSuccess: update,
+  });
+  return (
+    <button
+      disabled={isLoading}
+      onClick={() => {
+        const newPortions = Math.max(portions - 1, 1);
+        if (newPortions !== portions) {
+          mutate({ id, portions: Math.max(portions - 1, 1) });
+        }
+      }}
+    >
+      {isLoading ? (
+        <LoadingSpinner className="h-5" />
+      ) : (
+        <Icon
+          className="h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5"
+          icon="minus"
+        />
+      )}
+    </button>
   );
 };
 
