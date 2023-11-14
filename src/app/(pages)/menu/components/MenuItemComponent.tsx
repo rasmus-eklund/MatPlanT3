c0154b9dd8e942496572acd1debc5cf9360e6bf7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-14 17:01:48 +0100

    Fix: rescale menu item

--- src/app/(pages)/menu/components/MenuItemComponent.tsx
+++ src/app/(pages)/menu/components/MenuItemComponent.tsx
@@ -8,14 +8,18 @@ import LoadingSpinner from "~/app/_components/LoadingSpinner";
 type MenuItem = RouterOutputs["menu"]["getAll"][number];
 type Props = {
   item: MenuItem;
-  update: () => void;
 };
 
-const MenuItem = ({ item, update }: Props) => {
+const MenuItem = ({ item }: Props) => {
   const { id, name, portions } = item;
+  const utils = api.useUtils();
   const { mutate: remove, isLoading: removing } = api.menu.remove.useMutation({
-    onSuccess: update,
+    onSuccess: () => utils.menu.getAll.invalidate(),
   });
+  const { mutate: changePortions, isLoading: changingPortions } =
+    api.menu.changePortions.useMutation({
+      onSuccess: () => utils.menu.getAll.invalidate(),
+    });
   return (
     <li className="flex flex-col gap-2 rounded-md bg-c2 px-2 font-bold text-c5">
       <Link href={`/menu/${id}`} className="w-full">
@@ -23,9 +27,34 @@ const MenuItem = ({ item, update }: Props) => {
       </Link>
       <div className="flex w-full items-center justify-between gap-1">
         <div className="flex select-none items-center gap-1 p-2">
-          <Minus id={id} portions={portions} update={update} />
+          <button
+            disabled={changingPortions}
+            onClick={() => {
+              const newPortions = Math.max(portions - 1, 1);
+              if (newPortions !== portions) {
+                changePortions({ id, portions: Math.max(portions - 1, 1) });
+              }
+            }}
+          >
+            <Icon
+              className={`h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5 ${
+                changingPortions && "fill-c2"
+              }`}
+              icon="minus"
+            />
+          </button>
           <p className="text-lg">{portions}</p>
-          <Plus id={id} portions={portions} update={update} />
+          <button
+            disabled={changingPortions}
+            onClick={() => changePortions({ id, portions: portions + 1 })}
+          >
+            <Icon
+              className={`h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5 ${
+                changingPortions && "fill-c2"
+              }`}
+              icon="plus"
+            />
+          </button>
         </div>
         <button disabled={removing} onClick={() => remove({ id })}>
           {removing ? (
@@ -39,53 +68,6 @@ const MenuItem = ({ item, update }: Props) => {
         </button>
       </div>
     </li>
-  );
-};
-
-type ButtonProps = { id: string; portions: number; update: () => void };
-const Plus = ({ id, portions, update }: ButtonProps) => {
-  const { mutate, isLoading } = api.menu.changePortions.useMutation({
-    onSuccess: update,
-  });
-  return (
-    <button
-      disabled={isLoading}
-      onClick={() => mutate({ id, portions: portions + 1 })}
-    >
-      {isLoading ? (
-        <LoadingSpinner className="h-5" />
-      ) : (
-        <Icon
-          className="h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5"
-          icon="plus"
-        />
-      )}
-    </button>
-  );
-};
-const Minus = ({ id, portions, update }: ButtonProps) => {
-  const { mutate, isLoading } = api.menu.changePortions.useMutation({
-    onSuccess: update,
-  });
-  return (
-    <button
-      disabled={isLoading}
-      onClick={() => {
-        const newPortions = Math.max(portions - 1, 1);
-        if (newPortions !== portions) {
-          mutate({ id, portions: Math.max(portions - 1, 1) });
-        }
-      }}
-    >
-      {isLoading ? (
-        <LoadingSpinner className="h-5" />
-      ) : (
-        <Icon
-          className="h-6 w-6 fill-c4 md:hover:scale-110 md:hover:fill-c5"
-          icon="minus"
-        />
-      )}
-    </button>
   );
 };
 
