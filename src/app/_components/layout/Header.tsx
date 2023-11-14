38eb938b87e1f8aa8a75df74eb77f662c886945d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-14 16:50:10 +0100

    fix: only visible when logged in

--- src/app/_components/layout/Header.tsx
+++ src/app/_components/layout/Header.tsx
@@ -1,24 +1,30 @@
 import Image from "next/image";
 import NavLinks from "./NavLinks";
 import NavMenu from "./NavMenu";
+import { getServerAuthSession } from "~/server/auth";
 
 const Header = async () => {
+  const session = await getServerAuthSession();
   return (
     <>
-      <header className="sticky top-0 z-10 hidden items-center justify-between bg-c5 pr-2 md:flex">
-        <Image
-          className={"w-52"}
-          priority={true}
-          src={"/logo-color.svg"}
-          alt="MatPlan logo"
-          width={478}
-          height={222}
-        />
-        <nav className="grow px-5">
-          <NavLinks className="flex justify-evenly" icons="h-10" />
-        </nav>
-      </header>
-      <NavMenu />
+      {session && (
+        <>
+          <header className="sticky top-0 z-10 hidden items-center justify-between bg-c5 pr-2 md:flex">
+            <Image
+              className={"w-52"}
+              priority={true}
+              src={"/logo-color.svg"}
+              alt="MatPlan logo"
+              width={478}
+              height={222}
+            />
+            <nav className="grow px-5">
+              <NavLinks className="flex justify-evenly" icons="h-10" />
+            </nav>
+          </header>
+          <NavMenu />
+        </>
+      )}
     </>
   );
 };
