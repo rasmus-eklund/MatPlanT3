49cfd5618a00f6868979707a3ec0ff7276152bc1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-15 07:02:13 +0100

    Style: call to action

--- src/app/_components/Button.tsx
+++ src/app/_components/Button.tsx
@@ -1,7 +1,7 @@
 import type { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
 
 type ButtonProps = {
-  inverted?: boolean;
+  callToAction?: boolean;
   className?: string;
 } & DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
@@ -10,12 +10,12 @@ type ButtonProps = {
 
 export function Button({
   className: style = "",
-  inverted = false,
+  callToAction = false,
   ...props
 }: ButtonProps) {
-  const className = inverted
-    ? "bg-c1 md:hover:bg-c4 text-c5 md:hover:text-c2 border-c5 active:bg-c5"
-    : "bg-c4 md:hover:bg-c2 text-c1 md:hover:text-c5 border-c1 active:bg-c2";
+  const className = callToAction
+    ? "bg-c4 md:hover:bg-c2 text-c1 md:hover:text-c4 border-c1 active:bg-c2"
+    : "bg-c2 md:hover:bg-c4 text-c4 md:hover:text-c2 border-c1 active:bg-c2";
   return (
     <button
       className={`${className} ${style} rounded-md border-2 px-2 transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50`}
