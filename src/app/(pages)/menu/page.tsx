cbfde5614c901bb83f275351d143a0248b0f70bd
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-15 07:54:46 +0100

    Feat: only show days with a menu item and help text

--- src/app/(pages)/menu/page.tsx
+++ src/app/(pages)/menu/page.tsx
@@ -4,23 +4,47 @@ import MenuItemComponent from "~/app/(pages)/menu/components/MenuItemComponent";
 import days from "~/constants/days";
 import sortByName from "~/app/helpers/sortByName";
 import { api } from "~/trpc/react";
+import Icon from "~/icons/Icon";
+import { useRouter } from "next/navigation";
 
 const Menu = () => {
+  const router = useRouter();
   const { data: menu, isSuccess, isLoading } = api.menu.getAll.useQuery();
   return (
     <ul className="flex flex-col gap-2 p-2">
-      {days.map((day) => (
-        <li key={day} className="flex flex-col rounded-md bg-c3 p-2">
-          <h2 className="rounded-md p-2 text-xl font-bold text-c5">{day}</h2>
-          <ul className="flex flex-col gap-2">
-            {isSuccess &&
-              sortByName(menu.filter((i) => i.day === day)).map((item) => (
-                <MenuItemComponent key={item.id} item={item} />
-              ))}
-            {isLoading && <LoadingSpinner />}
-          </ul>
+      {isSuccess &&
+        days.map((day) => {
+          const menuItems = menu.filter((i) => i.day === day);
+          return (
+            <>
+              {!!menuItems.length && (
+                <li key={day} className="flex flex-col rounded-md bg-c3 p-2">
+                  <h2 className="rounded-md p-2 text-xl font-bold text-c5">
+                    {day}
+                  </h2>
+                  <ul className="flex flex-col gap-2">
+                    {sortByName(menuItems).map((item) => (
+                      <MenuItemComponent key={item.id} item={item} />
+                    ))}
+                  </ul>
+                </li>
+              )}
+            </>
+          );
+        })}
+      {isLoading && <LoadingSpinner />}
+      {isSuccess && !menu.length && (
+        <li
+          key="only-child"
+          className="flex flex-col items-center gap-1 self-center pt-20"
+        >
+          <p className="text-xl text-c2">Här var det tomt.</p>
+          <p> Klicka på besticken för att lägga till ett recept:</p>
+          <button onClick={() => router.push("/recipes/search")}>
+            <Icon className="h-10 animate-pulse fill-c3" icon="recipes" />
+          </button>
         </li>
-      ))}
+      )}
     </ul>
   );
 };
