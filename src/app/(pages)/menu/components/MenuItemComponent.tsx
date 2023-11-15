a4bb6bc74839190390a82a8a523ad247208e0160
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-15 07:59:11 +0100

    Style

--- src/app/(pages)/menu/components/MenuItemComponent.tsx
+++ src/app/(pages)/menu/components/MenuItemComponent.tsx
@@ -36,7 +36,7 @@ const MenuItem = ({ item }: Props) => {
       <Link href={`/menu/${id}`} className="w-full">
         {name}
       </Link>
-      <div className="flex w-full items-center justify-between gap-1">
+      <div className="flex w-full items-center justify-between md:justify-start gap-1">
         <div className="flex select-none items-center gap-1 p-2">
           <button
             disabled={changingPortions}
