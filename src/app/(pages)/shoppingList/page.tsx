79b4a7ab2c62ca2a4f0807f2d0065c9e667da87a
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-20 07:31:07 +0100

    Remove div

--- src/app/(pages)/shoppingList/page.tsx
+++ src/app/(pages)/shoppingList/page.tsx
@@ -3,11 +3,7 @@ import { api } from "~/trpc/server";
 
 const ShoppingListPage = async () => {
   const stores = await api.store.getAll.query();
-  return (
-    <div className="flex flex-col gap-2 p-2">
-      <Filters stores={stores} />
-    </div>
-  );
+  return <Filters stores={stores} />;
 };
 
 export default ShoppingListPage;
