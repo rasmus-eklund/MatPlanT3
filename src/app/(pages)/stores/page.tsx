7a9f46e37eec7695ec88fb6889b6620f081fb634
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-27 06:21:14 +0100

    use server

--- src/app/(pages)/stores/page.tsx
+++ src/app/(pages)/stores/page.tsx
@@ -1,24 +1,17 @@
-"use client";
 import AddNewStore from "~/app/(pages)/stores/components/AddNewStore";
 import StoreItem from "~/app/(pages)/stores/components/StoreItem";
-import LoadingSpinner from "~/app/_components/LoadingSpinner";
-import { api } from "~/trpc/react";
+import { api } from "~/trpc/server";
 
-const Stores = () => {
-  const {
-    data: stores,
-    isLoading,
-    isSuccess,
-    isError,
-  } = api.store.getAll.useQuery();
+const Stores = async () => {
+  const stores = await api.store.getAll.query();
 
   return (
     <div className="flex flex-col gap-2 rounded-md bg-c3 p-3">
       <h2 className="text-xl text-c5">Butiker</h2>
       <ul className="flex flex-col gap-2">
-        {isSuccess && stores.map((s) => <StoreItem key={s.id} store={s} />)}
-        {isLoading && <LoadingSpinner />}
-        {isError && <p>Något gick fel...</p>}
+        {stores.map((s) => (
+          <StoreItem key={s.id} store={s} />
+        ))}
         <li>
           <AddNewStore />
         </li>
