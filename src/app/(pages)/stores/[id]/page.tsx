f1745e02ce491f13342cfe34dc8b2d17a7114fb6
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-27 06:21:19 +0100

    use server

--- src/app/(pages)/stores/[id]/page.tsx
+++ src/app/(pages)/stores/[id]/page.tsx
@@ -1,24 +1,16 @@
-"use client";
 import SortableCategories from "~/app/(pages)/stores/components/SortableCategories";
 import StoreName from "~/app/(pages)/stores/components/StoreName";
 import LoadingSpinner from "~/app/_components/LoadingSpinner";
-import { api } from "~/trpc/react";
+import { api } from "~/trpc/server";
 
 type Props = { params: { id: string } };
-const Stores = ({ params: { id } }: Props) => {
-  const {
-    data: store,
-    isLoading,
-    isSuccess,
-    isError,
-  } = api.store.getById.useQuery({ id });
+const Stores = async ({ params: { id } }: Props) => {
+  const store = await api.store.getById.query({ id });
 
   return (
     <div className="flex flex-col gap-2 rounded-md bg-c3 p-3">
-      {isLoading && <LoadingSpinner />}
-      {isError && <p>Något gick fel...</p>}
-      {isSuccess && <StoreName id={id} name={store.name} />}
-      {isSuccess && <SortableCategories store={store} />}
+      <StoreName id={id} name={store.name} />
+      <SortableCategories store={store} />
     </div>
   );
 };
