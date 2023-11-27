a93c92202712453a14d36d809cb7dbb4375baaf7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-11-27 13:13:41 +0100

    style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,10 @@ import Header from "./_components/header/Header";
 import Footer from "./_components/Footer";
 import ProvidersWrapper from "./ProvidersWrapper";
 import { Toaster } from "react-hot-toast";
+import { env } from "~/env.mjs";
 
 export const metadata = {
-  title: "MatPlan",
+  title: env.NODE_ENV === "development" ? "DEV:MatPlan" : "MatPlan",
   description: "Plannera dina matinköp snabbt och smidigt",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
@@ -20,12 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={`flex h-full flex-col items-center font-sans bg-c3`}>
+      <body className={`flex h-full flex-col items-center bg-c3 font-sans`}>
         <TRPCReactProvider headers={headers()}>
           <ProvidersWrapper>
             <Toaster position="bottom-center" />
             <Header />
-            <main className="w-full max-w-5xl grow overflow-y-auto bg-c4 border-2 border-c5">
+            <main className="w-full max-w-5xl grow overflow-y-auto border-2 border-c5 bg-c4">
               {children}
             </main>
             <Footer />
