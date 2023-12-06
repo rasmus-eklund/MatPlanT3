a051efe81c510b2a7c1b607c0b7911f3fcf53cd2
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-06 14:13:52 +0100

    Feat: add recipe by link

--- src/server/api/routers/external.ts
+++ src/server/api/routers/external.ts
@@ -1,39 +1,23 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
-import axios from "axios";
-import { load } from "cheerio";
+import externalRecipes from "~/constants/externalRecipes";
+import { TRPCError } from "@trpc/server";
+import ICA from "~/server/helpers/scrape/ica";
+import ARLA from "~/server/helpers/scrape/arla";
 
 export const externalRouter = createTRPCRouter({
-  getICA: protectedProcedure
-    .input(z.object({ url: z.string().url() }))
-    .query(async ({ ctx, input: { url } }) => {
+  scrape: protectedProcedure
+    .input(z.object({ store: z.enum(["ICA", "ARLA"]), url: z.string().url() }))
+    .query(async ({ ctx, input: { url, store } }) => {
       const dbIngs = await ctx.db.ingredient.findMany({
         select: { name: true, id: true },
       });
-      const { data } = await axios.get(url);
-      const $ = load(data);
-      const recipeName = $("h1").text();
-      const ingredients: { quantity: number; unit?: string; name: string }[] =
-        [];
-      const couldNotMatch: { quantity: number; unit?: string; name: string }[] =
-        [];
-      const ingDiv = $(".ingredients-list-group.row-noGutter-column");
-      ingDiv.find(".ingredients-list-group__card").each((i, el) => {
-        const div = $(el);
-        const quantUnit = div
-          .find(".ingredients-list-group__card__qty")
-          .text()
-          .trim();
-        const match = quantUnit.match(/([\d\/]+)\s*([\s\S]*)/);
-        const quantityRaw = match ? match[1] : null;
-        const quantity = quantityRaw ? eval(quantityRaw) : null;
-        const unit = match ? match[2] : undefined;
-        const ingredientName = div
-          .find(".ingredients-list-group__card__ingr")
-          .text()
-          .trim();
-      });
-
-      return { name: recipeName, ingredients, couldNotMatch };
+      if (store === "ICA") {
+        return await ICA({ dbIngs, url });
+      }
+      if (store === "ARLA") {
+        return await ARLA({ dbIngs, url });
+      }
+      throw new TRPCError({ code: "BAD_REQUEST" });
     }),
 });
