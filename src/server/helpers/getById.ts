a0fdb0c60236ff10de505355c968b17dfe06e9cb
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-10 08:25:06 +0100

    dont try catch

--- src/server/helpers/getById.ts
+++ src/server/helpers/getById.ts
@@ -3,56 +3,49 @@ import { Unit } from "types";
 import { db } from "../db";
 
 export const getRecipeById = async (id: string) => {
-  try {
-    const recipe = await db.recipe.findUnique({
-      where: { id },
-      include: {
-        ingredients: {
-          include: { ingredient: { select: { name: true } } },
-          orderBy: { order: "asc" },
-        },
-        containers: {
-          select: {
-            id: true,
-            containedRecipeId: true,
-            portions: true,
-            containedRecipe: {
-              select: { name: true },
-            },
+  const recipe = await db.recipe.findUnique({
+    where: { id },
+    include: {
+      ingredients: {
+        include: { ingredient: { select: { name: true } } },
+        orderBy: { order: "asc" },
+      },
+      containers: {
+        select: {
+          id: true,
+          containedRecipeId: true,
+          portions: true,
+          containedRecipe: {
+            select: { name: true },
           },
         },
       },
-    });
-    if (!recipe) {
-      throw new TRPCError({
-        code: "NOT_FOUND",
-        message: "Recipe not found.",
-      });
-    }
-    const { ingredients, containers, ...rest } = recipe;
-    return {
-      recipe: rest,
-      contained: containers.map(
-        ({ containedRecipe: { name }, ...contained }) => ({
-          name,
-          ...contained,
-        }),
-      ),
-      ingredients: ingredients.map(
-        ({ recipeId, quantity, unit, ingredient, ...rest }) => {
-          return {
-            ...rest,
-            quantity: Number(quantity),
-            unit: unit as Unit,
-            name: ingredient.name,
-          };
-        },
-      ),
-    };
-  } catch (error) {
+    },
+  });
+  if (!recipe) {
     throw new TRPCError({
-      code: "BAD_REQUEST",
-      message: "Could not get recipe",
+      code: "NOT_FOUND",
+      message: "Recipe not found.",
     });
   }
+  const { ingredients, containers, ...rest } = recipe;
+  return {
+    recipe: rest,
+    contained: containers.map(
+      ({ containedRecipe: { name }, ...contained }) => ({
+        name,
+        ...contained,
+      }),
+    ),
+    ingredients: ingredients.map(
+      ({ recipeId, quantity, unit, ingredient, ...rest }) => {
+        return {
+          ...rest,
+          quantity: Number(quantity),
+          unit: unit as Unit,
+          name: ingredient.name,
+        };
+      },
+    ),
+  };
 };
