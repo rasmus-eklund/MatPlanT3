208388e9d2c3d40ee293b6d413f4aea0e25966ca
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-11 10:32:56 +0100

    Feat: add to menu component

---
+++ src/app/(pages)/recipes/search/_components/addToMenu.tsx
@@ -0,0 +1,34 @@
+"use client";
+import toast from "react-hot-toast";
+import Button from "~/app/_components/Button";
+import { api } from "~/trpc/react";
+
+type Props = { id: string };
+
+const AddToMenu = ({ id }: Props) => {
+  const utils = api.useUtils();
+  const { mutate: addToMenu, isLoading: addingToMenu } =
+    api.menu.addRecipe.useMutation({
+      onSuccess: () => {
+        utils.menu.getAll.invalidate();
+        toast.success("Recept tillagt!");
+      },
+      onError: (err) => {
+        console.log(err);
+        if (err.message === "Förbjuden cirkulär referens.") {
+          toast.error(err.message);
+        }
+      },
+    });
+  return (
+    <Button
+      callToAction
+      onClick={() => addToMenu({ id })}
+      disabled={addingToMenu}
+    >
+      Lägg till meny
+    </Button>
+  );
+};
+
+export default AddToMenu;
