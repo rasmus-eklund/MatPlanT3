881b783c42be234793676b9eb7f12516d37100ec
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-11 10:33:19 +0100

    Refactor: move add to menu to component

--- src/app/(pages)/recipes/search/_components/FoundRecipes.tsx
+++ src/app/(pages)/recipes/search/_components/FoundRecipes.tsx
@@ -1,9 +1,6 @@
-"use client";
 import Link from "next/link";
 import { RouterOutputs } from "~/trpc/shared";
-import { api } from "~/trpc/react";
-import toast from "react-hot-toast";
-import Button from "~/app/_components/Button";
+import AddToMenu from "./addToMenu";
 
 type Recipe = RouterOutputs["recipe"]["search"][number];
 
@@ -14,13 +11,6 @@ type Props = {
 
 const FoundRecipes = ({ recipe, shared }: Props) => {
   const { id, name, portions } = recipe;
-  const utils = api.useUtils();
-  const { mutate: add, isLoading: adding } = api.menu.addRecipe.useMutation({
-    onSuccess: async () => {
-      await utils.menu.getAll.invalidate();
-      toast.success("Recept tillagt!");
-    },
-  });
   return (
     <li className="flex flex-col rounded-md bg-c2 p-1 text-c5" key={id}>
       <Link
@@ -31,11 +21,7 @@ const FoundRecipes = ({ recipe, shared }: Props) => {
       </Link>
       <div className="flex w-full justify-between">
         <p className="text-c4">Port: {portions}</p>{" "}
-        {!shared && (
-          <Button callToAction disabled={adding} onClick={() => add(recipe)}>
-            Lägg till meny
-          </Button>
-        )}
+        {!shared && <AddToMenu id={id} />}
       </div>
     </li>
   );
