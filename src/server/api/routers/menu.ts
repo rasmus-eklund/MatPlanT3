4a8a701b8d7672794a5acfb5caab7cbf8353ba51
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-11 10:33:45 +0100

    Feat: Prevent circular references

--- src/server/api/routers/menu.ts
+++ src/server/api/routers/menu.ts
@@ -11,6 +11,7 @@ import { z } from "zod";
 import days from "~/constants/days";
 import scaleIngredients from "~/server/helpers/scaleIngredients";
 import formatQuantityUnit from "~/server/helpers/formatQuantityUnit";
+import ensureError from "~/server/helpers/ensureError";
 
 export const menuRouter = createTRPCRouter({
   addRecipe: protectedProcedure
@@ -18,31 +19,45 @@ export const menuRouter = createTRPCRouter({
     .mutation(async ({ ctx, input: { id } }) => {
       const userId = ctx.session.user.id;
       const recipe = await getRecipeById(id);
-      const allContained = await getAllContained(recipe);
-      const ingredients = [
-        ...recipe.ingredients.map(({ order, group, ...i }) => ({
-          ...i,
-          recipeId: recipe.recipe.id,
-        })),
-        ...allContained,
-      ];
-      await ctx.db.menu.create({
-        data: {
-          day: "Obestämd" as Day,
-          portions: recipe.recipe.portions,
-          userId,
-          recipeId: id,
-          shoppingListItem: {
-            createMany: {
-              data: ingredients.map(({ id, name, ...rest }) => ({
-                checked: false,
-                userId,
-                ...rest,
-              })),
+      try {
+        const allContained = await getAllContained(recipe, [id]);
+        const ingredients = [
+          ...recipe.ingredients.map(({ order, group, ...i }) => ({
+            ...i,
+            recipeId: recipe.recipe.id,
+          })),
+          ...allContained,
+        ];
+        await ctx.db.menu.create({
+          data: {
+            day: "Obestämd" as Day,
+            portions: recipe.recipe.portions,
+            userId,
+            recipeId: id,
+            shoppingListItem: {
+              createMany: {
+                data: ingredients.map(({ id, name, ...rest }) => ({
+                  checked: false,
+                  userId,
+                  ...rest,
+                })),
+              },
             },
           },
-        },
-      });
+        });
+      } catch (err) {
+        const error = ensureError(err);
+        if (error.message === "circular") {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Förbjuden cirkulär referens.",
+          });
+        }
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Något gick fel.",
+        });
+      }
     }),
 
   getAll: protectedProcedure.query(async ({ ctx }) => {
@@ -112,7 +127,10 @@ export const menuRouter = createTRPCRouter({
       const recipe = await getRecipeById(menuItem.recipeId);
       const scale = menuItem.portions / recipe.recipe.portions;
       recipe.ingredients = scaleIngredients(recipe.ingredients, scale);
-      const recipes = await getAllContainedRecipesRescaled(recipe);
+      recipe.recipe.portions *= scale;
+      const recipes = await getAllContainedRecipesRescaled(recipe, scale, [
+        recipe.recipe.id,
+      ]);
       return [recipe, ...recipes];
     }),
 });
