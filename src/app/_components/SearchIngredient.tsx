4a0d0b448b3a371ab5a872ae16748e6d06532405
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-12 11:07:56 +0100

    Keyboard interactive ingredient search

--- src/app/_components/SearchIngredient.tsx
+++ src/app/_components/SearchIngredient.tsx
@@ -9,64 +9,82 @@ type Props = { onSubmit: (ing: MeilIngredient) => void };
 const SearchIngredients = ({ onSubmit }: Props) => {
   const [search, setSearch] = useState("");
   const debouncedSearch = useDebounce(search, 500);
-
-  return (
-    <section className="relative flex flex-col align-middle">
-      <input
-        className="w-full rounded-md bg-c2 px-4 py-2 outline-none focus:bg-c1"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Lägg till vara..."
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-          }
-        }}
-        autoComplete="off"
-      />
-      {!!debouncedSearch && !!search && (
-        <Results
-          search={debouncedSearch}
-          onSelect={(ing) => {
-            setSearch("");
-            onSubmit(ing);
-          }}
-        />
-      )}
-    </section>
-  );
-};
-
-type ResultsProps = {
-  search: string;
-  onSelect: (ing: MeilIngredient) => void;
-};
-const Results = ({ search, onSelect }: ResultsProps) => {
+  const [selected, setSelected] = useState(0);
   const {
     data: ingredients,
     isError,
     isSuccess,
-  } = api.ingredient.search.useQuery({ search });
-
+  } = api.ingredient.search.useQuery(
+    { search: debouncedSearch },
+    { enabled: !!debouncedSearch },
+  );
   return (
-    <ul className="absolute top-10 z-50 w-full bg-c1">
-      {isSuccess &&
-        ingredients.map((ing) => (
-          <li
-            className={`cursor-pointer px-2 md:hover:bg-c3`}
-            key={ing.ingredientId}
-          >
-            <p
-              onClick={() => {
-                onSelect(ing);
-              }}
+    <section className="relative flex flex-col items-center">
+      <input
+        className="w-full rounded-md bg-c2 px-4 py-2 outline-none focus:bg-c1"
+        value={search}
+        onChange={({ target: { value } }) => setSearch(value)}
+        placeholder="Lägg till vara..."
+        onKeyDown={({ key }) => {
+          if (isSuccess) {
+            if (key === "Enter") {
+              const ing = ingredients[selected];
+              if (ing) {
+                setSearch("");
+                onSubmit(ing);
+              }
+            }
+            switch (key) {
+              case "ArrowDown":
+                setSelected((p) => {
+                  if (p === ingredients.length - 1) {
+                    return p;
+                  }
+                  return p + 1;
+                });
+                break;
+              case "ArrowUp":
+                setSelected((p) => {
+                  if (p === 0) {
+                    return p;
+                  }
+                  return p - 1;
+                });
+                break;
+
+              default:
+                break;
+            }
+          }
+        }}
+        autoComplete="off"
+      />
+      {isSuccess && !!debouncedSearch && (
+        <ul className="absolute top-full z-50 w-full rounded-md border border-c5 bg-c1">
+          {ingredients.map((ing, i) => (
+            <li
+              className={`cursor-pointer px-2 md:hover:bg-c3 ${
+                selected === i && "bg-c4 text-c1"
+              } ${i === 0 && "rounded-t-md"}`}
+              key={ing.ingredientId}
             >
-              {ing.name}
-            </p>
-          </li>
-        ))}
-      {isError && <li>Error</li>}
-    </ul>
+              <p
+                onClick={() => {
+                  onSubmit(ing);
+                }}
+              >
+                {ing.name}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+      {isError && (
+        <p className="w-full rounded-md border border-c5 bg-c1 p-1 text-c5">
+          Något gick fel...
+        </p>
+      )}
+    </section>
   );
 };
 
