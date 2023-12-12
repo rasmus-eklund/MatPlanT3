d84dc834bf839b1c706e6f242bc7633df0bbd1e0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-12 13:02:13 +0100

    label toggle

--- src/app/(pages)/recipes/search/_components/SearchRecipeForm.tsx
+++ src/app/(pages)/recipes/search/_components/SearchRecipeForm.tsx
@@ -31,15 +31,21 @@ const SearchRecipeForm = ({ query: { search: urlSearch, shared } }: Props) => {
         }}
         className="flex h-10 min-w-0 grow items-center justify-between gap-2 rounded-md bg-c2 pl-1 text-xl"
       >
-        <button
-          type="button"
-          className={`flex h-5 w-10 items-center rounded-lg border border-c5 px-[1px] ${
-            isPublic ? "justify-end" : "justify-start"
-          }`}
-          onClick={() => setIsPublic((p) => !p)}
-        >
-          <div className="h-4 w-4 rounded-full bg-c4"></div>
-        </button>
+        <div className="flex items-center gap-1">
+          <label className="w-8 text-xs text-c5" htmlFor="toggle-shared">
+            {isPublic ? "Delade" : "Egna"}
+          </label>
+          <button
+            type="button"
+            id="toggle-shared"
+            className={`flex h-5 w-10 items-center rounded-lg border border-c5 px-[1px] ${
+              isPublic ? "justify-end" : "justify-start"
+            }`}
+            onClick={() => setIsPublic((p) => !p)}
+          >
+            <div className="h-4 w-4 rounded-full bg-c4"></div>
+          </button>
+        </div>
         <input
           className="min-w-0 grow whitespace-nowrap bg-c2 outline-none"
           id="search-form-search"
@@ -47,7 +53,7 @@ const SearchRecipeForm = ({ query: { search: urlSearch, shared } }: Props) => {
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          placeholder={`Sök ${isPublic ? "delat recept" : ""}`}
+          placeholder="Sök"
         />
         <Icon className="h-10 fill-c3" icon="search" />
       </form>
