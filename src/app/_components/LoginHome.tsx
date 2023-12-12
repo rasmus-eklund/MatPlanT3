9b7f25607a586c3d2644ff268417a273cc7466c1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2023-12-12 06:18:09 +0100

    prettier

--- src/app/_components/LoginHome.tsx
+++ src/app/_components/LoginHome.tsx
@@ -4,7 +4,7 @@ import { signIn } from "next-auth/react";
 const Login = () => {
   return (
     <button
-      className="text-c2 bg-c5/80 md:hover:bg-c4 rounded-md p-2 px-6 text-3xl cursor-pointer"
+      className="cursor-pointer rounded-md bg-c5/80 p-2 px-6 text-3xl text-c2 md:hover:bg-c4"
       onClick={async () => {
         await signIn("google", { callbackUrl: "/menu" });
       }}
