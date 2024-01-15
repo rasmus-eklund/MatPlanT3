3e8cd600c551f3a985cb5509f49f98d81fabae4e
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-01-15 19:54:19 +0100

    Edit about

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -5,7 +5,6 @@ const Footer = () => {
     <footer className="flex min-h-[25px] w-full max-w-5xl justify-between bg-c5 px-2 text-c4">
       <p>&copy; Rasmus Eklund</p>
       <div className="flex items-center gap-4">
-        <Link href={"/about"}>Donate</Link>
         <a
           className="hover:text-c2"
           href="https://github.com/rasmus-eklund"
