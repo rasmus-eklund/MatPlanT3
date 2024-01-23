e3f29b9e798735806e92dc9d6c58a9ef7b942a54
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-01-23 09:59:00 +0100

    Add privacy policy

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 const About = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center gap-8 px-2 py-10">
+    <main className="flex h-full w-full flex-col items-center gap-8 px-2 py-10">
       <h1 className="text-4xl text-c1">MatPlan</h1>
-    </div>
+      <Link href={"/privacy"}>Privacy policy</Link>
+    </main>
   );
 };
 
---
+++ src/app/(pages)/privacy/page.tsx
@@ -0,0 +1,96 @@
+const Privacy = () => {
+  return (
+    <main>
+      <section className="flex flex-col gap-4 p-5 text-c1">
+        <h2 className="text-4xl">Privacy Policy for MatPlan</h2>
+
+        <p>Effective Date: 2024-01-23</p>
+
+        <p>
+          Thank you for using MatPlan! This Privacy Policy is meant to help you
+          understand what information we collect, why we collect it, and how we
+          use and safeguard your information.
+        </p>
+
+        <h2 className="text-lg">1. Information We Collect</h2>
+
+        <h3 className="font-bold">1.1 User Authentication:</h3>
+        <p>
+          When you use MatPlan, you have the option to log in using your Google
+          account. We collect and store your email address for authentication
+          purposes.
+        </p>
+
+        <h3>1.2 Recipe Data:</h3>
+        <p>
+          MatPlan allows you to store and manage your recipes. The content of
+          your recipes is stored securely on our servers.
+        </p>
+
+        <h2 className="text-lg">2. How We Use Your Information</h2>
+
+        <h3>2.1 User Authentication:</h3>
+        <p>
+          Your email address is used solely for authentication purposes. We do
+          not use this information for any other purpose.
+        </p>
+
+        <h3>2.2 Recipe Data:</h3>
+        <p>
+          The recipe data you provide is used to enable the features of MatPlan,
+          such as saving, organizing, and retrieving your recipes.
+        </p>
+
+        <h2 className="text-lg">3. Data Security</h2>
+
+        <p>
+          We take the security of your information seriously. We implement
+          industry-standard security measures to protect your data from
+          unauthorized access, disclosure, alteration, and destruction.
+        </p>
+
+        <h2 className="text-lg">4. Data Retention</h2>
+
+        <p>
+          We retain your email address for as long as your account is active.
+          Recipe data is retained as long as your account exists. You can delete
+          your account at any time, and all associated data will be permanently
+          removed.
+        </p>
+
+        <h2 className="text-lg">5. Third-Party Services</h2>
+
+        <p>
+          MatPlan uses Google for user authentication. Please refer to Google's
+          privacy policy for information on how they handle user data.
+        </p>
+
+        <h2 className="text-lg">6. Children's Privacy</h2>
+
+        <p>
+          MatPlan is not intended for children under the age of 13. We do not
+          knowingly collect or store personal information from children under
+          13. If you believe we have inadvertently collected such information,
+          please contact us immediately.
+        </p>
+
+        <h2 className="text-lg">7. Changes to This Privacy Policy</h2>
+
+        <p>
+          We reserve the right to update or modify this Privacy Policy at any
+          time. You will be notified of any changes through the application or
+          via email.
+        </p>
+
+        <h2 className="text-lg">8. Contact Us</h2>
+
+        <p>
+          If you have any questions or concerns about this Privacy Policy,
+          please contact us at eklund.rasmus44@gmail.com.
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default Privacy;
