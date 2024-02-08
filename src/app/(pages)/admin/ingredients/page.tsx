abead5eb54f9c5e6a41116a5267905e3bc0ed834
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-08 13:22:25 +0100

    Refactor: use server component

--- src/app/(pages)/admin/ingredients/page.tsx
+++ src/app/(pages)/admin/ingredients/page.tsx
@@ -1,26 +1,22 @@
-"use client";
-import { useSession } from "next-auth/react";
 import ShowIngredients from "~/app/(pages)/admin/ingredients/_components/ShowIngredients";
-import { api } from "~/trpc/react";
+import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
+import AdminHeader from "../_components/AdminHeader";
 
-const Ingredients = () => {
-  const { data: session } = useSession();
+const Ingredients = async () => {
+  const session = await getServerAuthSession();
   if (session && session.user.role !== "ADMIN") {
-    return (
-      <>
-        <p>Det finns inget för dig här.</p>
-      </>
-    );
+    return <p>Det finns inget för dig här.</p>;
   }
-
-  const { data: ingredients, isSuccess: gotIngs } = api.admin.getAll.useQuery();
-  const { data: allCats, isSuccess: gotCats } = api.admin.categories.useQuery();
+  const [ingredients, allCats] = await Promise.all([
+    api.admin.getAll.query(),
+    api.admin.categories.query(),
+  ]);
   return (
-    <>
-      {gotIngs && gotCats && (
-        <ShowIngredients ingredients={ingredients} allCats={allCats} />
-      )}
-    </>
+    <div>
+      <AdminHeader />
+      <ShowIngredients ingredients={ingredients} allCats={allCats} />
+    </div>
   );
 };
 
