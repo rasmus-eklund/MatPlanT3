05e062f6b93c313eabcc54ec1478c74be9b81769
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-08 13:21:32 +0100

    Refactor: Move header to component

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,37 +1,21 @@
-import Link from "next/link";
-import Icon from "~/icons/Icon";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
 import UpdateMeilisearchButton from "./_components/UpdateMeilisearchButton";
+import AdminHeader from "./_components/AdminHeader";
 
 const Admin = async () => {
   const session = await getServerAuthSession();
   if (session && session.user.role !== "ADMIN") {
-    return (
-      <>
-        <p>Det finns inget för dig här.</p>
-      </>
-    );
+    return <p>Det finns inget för dig här.</p>;
   }
   const nrUsers = await api.users.getCount.query();
   return (
-    <div className="h-full bg-c1">
-      <header className="bg-c2">
-        <ul className="flex">
-          <li>
-            <Link href="/admin/ingredients">
-              <Icon icon="pizza" className="w-10 fill-c5" />
-            </Link>
-          </li>
-          <li>
-            <Link href="/admin/users">
-              <Icon icon="admin" className="w-10 fill-c5" />
-            </Link>
-          </li>
-        </ul>
-      </header>
-      <p>Users: {nrUsers}</p>
-      <UpdateMeilisearchButton />
+    <div className="bg-c3">
+      <AdminHeader />
+      <div className="flex flex-col gap-5 p-5">
+        <p>Antal användare: {nrUsers}</p>
+        <UpdateMeilisearchButton />
+      </div>
     </div>
   );
 };
