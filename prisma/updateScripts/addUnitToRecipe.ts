3e453ffb99730fe0bf9b2cb99cbc3466e562955d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-22 14:49:29 +0100

    copy old values to new schema variables

---
+++ prisma/updateScripts/addUnitToRecipe.ts
@@ -0,0 +1,71 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+/*
+1. push to db:
+
+	model recipe
+	quantity         Int?
+    unit             String?
+	
+	model recipe_recipe
+	quantity          Decimal?
+	
+	model menu
+	quantity          Decimal?
+	
+	model store_category
+	store not optional
+	storeId not optional
+	
+2. run scripts
+
+3. push to db:
+
+	model recipe
+	- rm portions
+	- quant, unit not optional
+	
+	model recipe_recipe, menu
+	- rm portions
+	- quant not optional
+
+4. Reseed meilisearch recipes
+*/
+
+type DB = PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+export const addQuantUnitToRecipe = async (db: DB) => {
+  const recipes = await db.recipe.findMany();
+  await Promise.all(
+    // @ts-ignore
+    recipes.map(({ id, portions }) =>
+      db.recipe.update({
+        where: { id },
+        data: { quantity: portions, unit: "port" },
+      }),
+    ),
+  );
+  console.log("Successfully added quantity and unit to recipes.");
+};
+
+export const addQuantityToRecipeRecipe = async (db: DB) => {
+  const recipes = await db.recipe_recipe.findMany();
+  await Promise.all(
+    // @ts-ignore
+    recipes.map(({ id, portions }) =>
+      db.recipe_recipe.update({ where: { id }, data: { quantity: portions } }),
+    ),
+  );
+  console.log("Successfully added quantity to recipe_recipe.");
+};
+
+export const addQuantityToMenu = async (db: DB) => {
+  const menu = await db.menu.findMany();
+  await Promise.all(
+    // @ts-ignore
+    menu.map(({ id, portions }) =>
+      db.menu.update({ where: { id }, data: { quantity: portions } }),
+    ),
+  );
+  console.log("Successfully added quantity to menu.");
+};
