9fd0bff056470ebe9db7ddf5ef62a3be3a359169
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-22 14:57:12 +0100

    add conversion

--- src/constants/units.ts
+++ src/constants/units.ts
@@ -15,4 +15,21 @@ const units = [
   "port",
 ] as const;
 
+export const unitsAbbr = {
+  st: "Antal",
+  g: "Gram",
+  kg: "Kilogram",
+  dl: "Deciliter",
+  krm: "Kryddmått",
+  tsk: "Tesked",
+  msk: "Matsked",
+  ml: "Milliliter",
+  cl: "Centiliter",
+  l: "Liter",
+  klyfta: "Klyftor",
+  pkt: "Paket",
+  burk: "Burkar",
+  port: "Portioner",
+} as const;
+
 export default units;
