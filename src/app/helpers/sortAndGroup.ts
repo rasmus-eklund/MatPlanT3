ffaaaa6754917427943a73dfd5274f1cce4dba55
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-23 17:26:56 +0100

    rename file

--- src/app/helpers/sortAndGroup.ts
+++ src/app/helpers/sortAndGroup.ts
@@ -1,4 +1,26 @@
-import { tItem, tItemsGrouped, StoreOrder } from "types";
+import { tItem, tItemsGrouped, StoreOrder, CategoryItem } from "types";
+import type { RouterOutputs } from "~/trpc/shared";
+type Items = RouterOutputs["store"]["getById"]["order"];
+
+export const groupSubcategoryByCategory = (items: Items): CategoryItem[] => {
+  const start: CategoryItem[] = [];
+  return items.reduce((acc, inputItem) => {
+    const foundIndex = acc.findIndex(
+      (item) => item.id === inputItem.category.id,
+    );
+    if (foundIndex === -1) {
+      acc.push({
+        ...inputItem.category,
+        subcategories: [{ ...inputItem.subcategory }],
+      });
+    } else {
+      acc[foundIndex]!.subcategories.push({
+        ...inputItem.subcategory,
+      });
+    }
+    return acc;
+  }, start);
+};
 
 export const sortBySubcategory = <
   T extends { name: string; subcategoryId: number },
@@ -57,3 +79,6 @@ export const groupByUnit = (items: tItem[]) => {
     return acc;
   }, start);
 };
+
+export const sortByName = <T extends { name: string }>(items: T[]) =>
+  items.sort((a, b) => a.name.localeCompare(b.name));
