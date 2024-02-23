e2591b1fe07c5546d3fdd1167c760443f05d69d4
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-02-23 17:27:18 +0100

    move function

--- src/app/helpers/sortByName.ts
+++
@@ -1,9 +0,0 @@
-const sortByName = <T extends { name: string }>(items: T[]) => {
-  return items.sort((a, b) => {
-    if (a.name < b.name) return -1;
-    if (a.name > b.name) return 1;
-    return 0;
-  });
-};
-
-export default sortByName;
