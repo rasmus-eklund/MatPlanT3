1d601b8492bf3c60f6027f94baa94ac3f8637073
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-19 16:10:47 +0200

    Refactor: reusable authorize function

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,12 +1,25 @@
 "use server";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import type { UserSession } from "./shared";
+import { errorMessages } from "./errors";
 
 export const getServerAuthSession = async () => {
   const { getUser, getPermission } = getKindeServerSession();
   const admin = await getPermission("is:admin");
   const user = await getUser();
   if (user) {
-    return { ...user, admin: admin?.isGranted ?? false };
+    const { id, ...rest } = user;
+    return { authId: id, ...rest, admin: admin?.isGranted ?? false };
   }
   return null;
+};
+
+export const authorize = async (
+  admin = false,
+): Promise<NonNullable<UserSession>> => {
+  const user = await getServerAuthSession();
+  if (!user || (admin && !user.admin)) {
+    throw new Error(errorMessages.UNAUTHORIZED);
+  }
+  return user;
 };
