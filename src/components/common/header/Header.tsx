fe722998ee3e8e2f07e06db61b1bacc6a101ebed
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-19 10:43:30 +0200

    Refactor: header

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,12 +1,22 @@
-import NavMenu from "./NavMenu";
 import { getServerAuthSession } from "~/server/auth";
+import NavLinks from "./NavLinks";
+import Image from "next/image";
 
 const Header = async () => {
   const user = await getServerAuthSession();
-  if (user) {
-    return <NavMenu admin={user.admin} />;
-  }
-  return null;
+  return (
+    <header className="bg-c5 sticky top-0 z-10 flex w-full max-w-5xl items-center justify-between pr-2">
+      <Image
+        className={"w-28 md:w-52"}
+        priority={true}
+        src={"/logo-color.svg"}
+        alt="MatPlan logo"
+        width={478}
+        height={222}
+      />
+      <NavLinks user={user} />
+    </header>
+  );
 };
 
 export default Header;
--- src/components/common/header/NavLinks.tsx
+++ src/components/common/header/NavLinks.tsx
@@ -1,41 +1,74 @@
 "use client";
 import Link from "next/link";
 import Icon from "~/icons/Icon";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import type { UserSession } from "~/server/shared";
 
-type Props = { className: string; icons?: string; admin: boolean };
-const NavLinks = ({ className: style, icons, admin }: Props) => {
+type Props = { user: UserSession | null };
+const NavLinks = ({ user }: Props) => {
   const items = [
     { name: "Meny", href: "/menu", icon: "home" },
     { name: "Maträtter", href: "/recipes/search", icon: "recipes" },
     { name: "Varor", href: "/items", icon: "pizza" },
     { name: "Inköpslista", href: "/shoppingList", icon: "cart" },
+  ] as const;
+  const menuItems = [
     { name: "Butik", href: "/stores", icon: "store" },
     { name: "Profil", href: "/user", icon: "user" },
   ] as const;
   const className = {
-    icon: `fill-c3 size-10 md:hover:scale-110 ${icons}`,
+    icon: "fill-c3 size-10 md:size-12",
     title: "text-sm font-bold text-c2 md:text-base hidden md:block",
-    parent: "flex flex-col items-center",
+    parent: "flex flex-col items-center md:hover:scale-110 justify-between",
+    menuIcon: "fill-c3 size-5",
   };
   return (
-    <ul className={style}>
-      {items.map(({ name, href, icon }) => (
-        <li key={name}>
-          <Link className={className.parent} href={href} data-cy={icon}>
-            <Icon className={className.icon} icon={icon} />
-            <h3 className={className.title}>{name}</h3>
-          </Link>
-        </li>
-      ))}
-      {admin && (
-        <li>
-          <Link prefetch={false} className={className.parent} href={"/admin"}>
-            <Icon className={className.icon} icon="admin" />
-            <h3 className={className.title}>Admin</h3>
-          </Link>
-        </li>
-      )}
-    </ul>
+    <nav className="flex grow justify-between gap-4">
+      <ul className="flex grow items-center justify-evenly px-4">
+        {items.map(({ name, href, icon }) => (
+          <li key={name + " nav"}>
+            <Link className={className.parent} href={href} data-cy={icon}>
+              <Icon className={className.icon} icon={icon} />
+              <h3 className={className.title}>{name}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <Icon icon="hamburgerMenu" className={className.icon} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            {user?.given_name ?? "Ditt"} konto
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {menuItems.map(({ href, icon, name }) => (
+            <DropdownMenuItem asChild key={name + " menu"}>
+              <Link href={href} className="flex gap-4">
+                <Icon className={className.menuIcon} icon={icon} />
+                <span>{name}</span>
+              </Link>
+            </DropdownMenuItem>
+          ))}
+          {user?.admin && (
+            <DropdownMenuItem asChild>
+              <Link className="flex gap-4" href={"/admin"}>
+                <Icon className={className.menuIcon} icon="admin" />
+                <span>Admin</span>
+              </Link>
+            </DropdownMenuItem>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </nav>
   );
 };
 
