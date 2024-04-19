460a3d82138ac7eff091029348f22856e3280c8d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-19 15:52:19 +0200

    Handle on server

--- src/app/(pages)/admin/page.tsx
+++ src/app/(pages)/admin/page.tsx
@@ -1,12 +1,7 @@
 import UpdateMeilisearchButton from "./_components/UpdateMeilisearchButton";
-import { getServerAuthSession } from "~/server/auth";
 import { getUserCount } from "~/server/api/admin";
 
 const Admin = async () => {
-  const user = await getServerAuthSession();
-  if (!user?.admin) {
-    return <p>Det finns inget för dig här.</p>;
-  }
   const nrUsers = await getUserCount();
   return (
     <div className="flex flex-col gap-5 p-5">
