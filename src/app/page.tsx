1268d8c37f9bc520d0cdb37c92742dd1daabfa8b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-19 15:51:59 +0200

    Remove login link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
-
 export default function HomePage() {
   return (
     <div>
       <h2>Välkommen</h2>
-      <Link href={"/login"}>Logga in</Link>
     </div>
   );
 }
