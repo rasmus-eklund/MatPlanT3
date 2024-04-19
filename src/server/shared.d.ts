316404ccfe9d29ee8388101659df1a184e90098b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-19 16:10:58 +0200

    Add: Return type exports

--- src/server/shared.d.ts
+++ src/server/shared.d.ts
@@ -1,8 +1,13 @@
 import type { getAllIngredients, getAllCategories } from "./api/admin";
+import type { getUserStats, getAllUsers } from "./api/users";
 import type { getServerAuthSession } from "./auth";
 
-export type GetAllIngredients = Awaited<ReturnType<typeof getAllIngredients>>;
+export type AllIngredients = Awaited<ReturnType<typeof getAllIngredients>>;
 
-export type GetAllCategories = Awaited<ReturnType<typeof getAllCategories>>;
+export type AllCategories = Awaited<ReturnType<typeof getAllCategories>>;
+
+export type UserStats = Awaited<ReturnType<typeof getUserStats>>;
 
 export type UserSession = Awaited<ReturnType<typeof getServerAuthSession>>;
+
+export type AllUsers = Awaited<ReturnType<typeof getAllUsers>>;
