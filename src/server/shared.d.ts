7896597350ff81bdcf63a3aad55829747bbf3a9b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-19 10:43:51 +0200

    Shared session type

--- src/server/shared.d.ts
+++ src/server/shared.d.ts
@@ -1,5 +1,8 @@
 import type { getAllIngredients, getAllCategories } from "./api/admin";
+import type { getServerAuthSession } from "./auth";
 
 export type GetAllIngredients = Awaited<ReturnType<typeof getAllIngredients>>;
 
 export type GetAllCategories = Awaited<ReturnType<typeof getAllCategories>>;
+
+export type UserSession = Awaited<ReturnType<typeof getServerAuthSession>>;
