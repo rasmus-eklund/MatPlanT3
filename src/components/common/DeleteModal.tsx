6693b8c452ba3f13d6524ace1246921cfd2efc69
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-21 06:59:42 +0200

    Add reusable delete modal

---
+++ src/components/common/DeleteModal.tsx
@@ -0,0 +1,54 @@
+import Icon from "~/icons/Icon";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
+import { Button } from "~/components/ui/button";
+import ServerFormSubmit from "./ServerFormSubmit";
+
+type Props = {
+  info: {
+    name: string;
+    description: string;
+  };
+  icon: boolean;
+  action: () => Promise<void>;
+};
+
+const RemoveItemDialog = ({ icon = true, info, action }: Props) => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {icon ? (
+          <Icon icon="delete" className="size-6 fill-c5 hover:scale-110" />
+        ) : (
+          <Button variant="destructive">Ta bort</Button>
+        )}
+      </DialogTrigger>
+      <DialogContent className="bg-c2">
+        <DialogHeader>
+          <DialogTitle>Ta bort {info.name}</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>{info.description}</DialogDescription>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button type="button" variant="secondary">
+              Avbryt
+            </Button>
+          </DialogClose>
+          <form action={action}>
+            <ServerFormSubmit>Ta bort</ServerFormSubmit>
+          </form>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default RemoveItemDialog;
