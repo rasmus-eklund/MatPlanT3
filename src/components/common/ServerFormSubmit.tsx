92ac000a9e3b6983060a8432a73bdcfd9e918f71
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-21 07:00:02 +0200

    Add Submit button for server forms

---
+++ src/components/common/ServerFormSubmit.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { type ReactNode } from "react";
+import { useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+import { ClipLoader } from "react-spinners";
+import Icon, { type tIcon } from "~/icons/Icon";
+
+type Props = { children: ReactNode; icon?: tIcon | null };
+
+const ServerFormSubmit = ({ children, icon, ...props }: Props) => {
+  const { pending } = useFormStatus();
+  if (icon) {
+    if (pending) {
+      <ClipLoader size={20} />;
+    }
+    return <Icon icon={icon} className="size-6" />;
+  } else {
+    if (pending) {
+      return (
+        <Button type="button" disabled>
+          <ClipLoader size={20} className="mr-2" />
+          Vänta
+        </Button>
+      );
+    }
+    return (
+      <Button type="submit" {...props} disabled={pending}>
+        {children}
+      </Button>
+    );
+  }
+};
+
+export default ServerFormSubmit;
