bd0af3803fd1cfc40f95588ed7748a53be9adba0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-21 07:02:22 +0200

    Rename file

--- src/server/meilisearch/MeiliDockerSetup.sh
+++ src/server/meilisearch/MeiliDockerSetup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Fetch the latest version of Meilisearch image from DockerHub
 docker pull getmeili/meilisearch:v1.7
 
@@ -5,6 +7,7 @@ docker pull getmeili/meilisearch:v1.7
 docker run -it --rm \
     -p 7700:7700 \
     -e MEILI_ENV='development' \
+    -e MEILI_MASTER_KEY='masterkey' \
     -v $(pwd)/meili_data:/meili_data \
     getmeili/meilisearch:v1.7
 # Use ${pwd} instead of $(pwd) in PowerShell
