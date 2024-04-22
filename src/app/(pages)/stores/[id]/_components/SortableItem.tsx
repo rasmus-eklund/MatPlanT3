715afbd1c8cf0f25ffb937410c08f52f79754f47
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-22 13:13:45 +0200

    sortable item

---
+++ src/app/(pages)/stores/[id]/_components/SortableItem.tsx
@@ -0,0 +1,31 @@
+"use client";
+import { useSortable } from "@dnd-kit/sortable";
+import type { ReactNode } from "react";
+import { CSS } from "@dnd-kit/utilities";
+
+type UseSortableReturn = Omit<
+  ReturnType<typeof useSortable>,
+  "setNodeRef" | "transform" | "transition"
+>;
+
+type SortableItemProps = {
+  id: string;
+  children: (args: UseSortableReturn) => ReactNode;
+};
+
+const SortableItem = (props: SortableItemProps) => {
+  const { setNodeRef, transform, transition, ...rest } = useSortable({
+    id: props.id,
+  });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children({ ...rest })}
+    </div>
+  );
+};
+
+export default SortableItem;
