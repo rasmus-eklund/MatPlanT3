decd9f21e50baff05fc0c28c90d74b659737528c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-22 13:19:27 +0200

    Refactor: store return, implement rename store, reorder store items and add create new store

--- src/server/api/stores.ts
+++ src/server/api/stores.ts
@@ -3,11 +3,13 @@
 import { revalidatePath } from "next/cache";
 import { authorize } from "../auth";
 import { db } from "../db";
-import { store } from "../db/schema";
+import { store, store_category, store_subcategory } from "../db/schema";
 import { and, eq } from "drizzle-orm";
-import { createNewStore } from "~/lib/utils/createNewStore";
 import { type tName } from "~/zod/zodSchemas";
 import { notFound } from "next/navigation";
+import { errorMessages } from "../errors";
+import { randomUUID } from "crypto";
+import type { CategoryItem } from "~/types";
 
 export const getAllStores = async () => {
   const user = await authorize();
@@ -28,10 +30,15 @@ export const getStoreById = async (id: string) => {
     },
     with: {
       store_categories: {
-        columns: {},
+        columns: { id: true, order: true },
+        orderBy: store_category.order,
         with: {
           category: true,
-          subcategory: { columns: { categoryId: false } },
+          store_subcategories: {
+            columns: { id: true, order: true },
+            orderBy: store_subcategory.order,
+            with: { subcategory: { columns: { id: true, name: true } } },
+          },
         },
       },
     },
@@ -40,42 +47,134 @@ export const getStoreById = async (id: string) => {
   if (!foundStore) {
     notFound();
   }
-
-  return foundStore;
+  const { store_categories, ...rest } = foundStore;
+  const categories = store_categories.map(
+    ({ id, category: { name }, store_subcategories, order }) => ({
+      id: id.toString(),
+      name,
+      order,
+      subcategories: store_subcategories.map(
+        ({ id, subcategory: { name }, order }) => ({
+          id: id.toString(),
+          name,
+          order,
+        }),
+      ),
+    }),
+  );
+  return { ...rest, categories };
 };
 
 export const addStore = async ({ name }: tName) => {
   const user = await authorize();
-  await createNewStore({ userId: user.id, name });
-  revalidatePath("/stores");
+  try {
+    await createNewStore({ userId: user.id, name });
+    revalidatePath("/stores");
+  } catch (error) {
+    throw new Error(errorMessages.FAILEDINSERT);
+  }
 };
 
 export const deleteStore = async (id: string) => {
   const user = await authorize();
-  await db
-    .delete(store)
-    .where(and(eq(store.id, id), eq(store.userId, user.id)));
-  revalidatePath("/stores");
+  try {
+    await db
+      .delete(store)
+      .where(and(eq(store.id, id), eq(store.userId, user.id)));
+    revalidatePath("/stores");
+  } catch (error) {
+    throw new Error(errorMessages.FAILEDINSERT);
+  }
 };
 
-//   rename: protectedProcedure
-//     .input(zNameId)
-//     .mutation(({ ctx, input: { name, id } }) =>
-//       ctx.db.store.update({ where: { id }, data: { name } }),
-//     ),
+export const renameStore = async ({
+  id,
+  name,
+}: {
+  id: string;
+  name: string;
+}) => {
+  const user = await authorize();
+  try {
+    await db
+      .update(store)
+      .set({ name })
+      .where(and(eq(store.id, id), eq(store.userId, user.id)));
+    revalidatePath("/stores");
+  } catch (error) {
+    throw new Error(errorMessages.FAILEDINSERT);
+  }
+};
 
-//   updateOrder: protectedProcedure
-//     .input(z.object({ storeId: z.string().min(1), data: zStoreOrder }))
-//     .mutation(async ({ ctx, input: { storeId, data } }) => {
-//       const userId = ctx.session.user.id;
-//       await ctx.db.store.update({
-//         where: { id: storeId, userId },
-//         data: {
-//           order: {
-//             deleteMany: { storeId },
-//             createMany: { data },
-//           },
-//         },
-//       });
-//     }),
-// });
+type CreateNewStoreProps = { userId: string; name: string };
+export const createNewStore = async ({ name, userId }: CreateNewStoreProps) => {
+  const categories = await db.query.category.findMany({
+    with: { subcategories: true },
+  });
+
+  const storeId = randomUUID();
+  type StoreInsert = typeof store.$inferInsert;
+  const newStore: StoreInsert = { id: storeId, name, userId };
+  type StoreCategoryInsert = typeof store_category.$inferInsert;
+  const newStoreCategories: StoreCategoryInsert[] = [];
+  type StoreSubcategoryInsert = typeof store_subcategory.$inferInsert;
+  const newStoreSubcategories: StoreSubcategoryInsert[] = [];
+
+  let catOrder = 0;
+  for (const { id: categoryId, subcategories } of categories) {
+    const store_categoryId = randomUUID();
+    newStoreCategories.push({
+      id: store_categoryId,
+      categoryId,
+      order: catOrder,
+      storeId,
+    });
+    catOrder++;
+    let subCatOrder = 0;
+    for (const { id: subcategoryId } of subcategories) {
+      newStoreSubcategories.push({
+        order: subCatOrder,
+        store_categoryId,
+        subcategoryId,
+      });
+      subCatOrder++;
+    }
+  }
+  try {
+    await db.insert(store).values(newStore);
+    await db.insert(store_category).values(newStoreCategories);
+    await db.insert(store_subcategory).values(newStoreSubcategories);
+  } catch (error) {
+    throw new Error(errorMessages.FAILEDINSERT);
+  }
+};
+
+type UpdateStoreOrderProps = {
+  categories: CategoryItem[];
+  subcategories: (CategoryItem["subcategories"][number] & {
+    categoryId: string;
+  })[];
+  storeId: string;
+};
+export const updateStoreOrder = async ({
+  categories,
+  subcategories,
+  storeId,
+}: UpdateStoreOrderProps) => {
+  await authorize();
+  await db.transaction(async (tx) => {
+    for (const { id, order } of categories) {
+      await tx
+        .update(store_category)
+        .set({ order })
+        .where(eq(store_category.id, id));
+    }
+    for (const { id, order, categoryId } of subcategories) {
+      await tx
+        .update(store_subcategory)
+        .set({ order, store_categoryId: categoryId })
+        .where(eq(store_subcategory.id, id));
+    }
+  });
+  revalidatePath(`/stores/${storeId}`);
+};
