5aaac8f2bad9c95f649b547a3cfb5005284a200e
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-22 13:18:26 +0200

    add return for store with items

--- src/server/shared.d.ts
+++ src/server/shared.d.ts
@@ -1,5 +1,5 @@
 import type { getAllIngredients, getAllCategories } from "./api/admin";
-import type { getAllStores } from "./api/stores";
+import type { getAllStores, getStoreById } from "./api/stores";
 import type { getUserStats, getAllUsers } from "./api/users";
 import type { getServerAuthSession } from "./auth";
 
@@ -14,3 +14,5 @@ export type UserSession = Awaited<ReturnType<typeof getServerAuthSession>>;
 export type AllUsers = Awaited<ReturnType<typeof getAllUsers>>;
 
 export type Store = Awaited<ReturnType<typeof getAllStores>>;
+
+export type StoreWithItems = Awaited<ReturnType<typeof getStoreById>>;
