70238001642b36f40e2ffe6c27cadccfade4aa75
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-22 13:14:28 +0200

    toast on success

--- src/app/(pages)/user/_components/Settings.tsx
+++ src/app/(pages)/user/_components/Settings.tsx
@@ -1,14 +1,22 @@
+import { toast } from "sonner";
 import DeleteUser from "./DeleteUser";
 import EditNameDialog from "~/components/common/EditNameDialog";
+import { renameUser } from "~/server/api/users";
+import type { tName } from "~/zod/zodSchemas";
 
 type Props = { name: string | null; id: string };
 const Settings = ({ name, id }: Props) => {
+  const onSubmit = async ({ name }: tName) => {
+    await renameUser(name);
+    toast.success("Användarnamn bytt!");
+  };
   return (
     <div className="flex flex-col gap-2 bg-c2 p-5">
       <p>Användarnamn: {name ?? "Inget namn"}</p>
       <EditNameDialog
         name={name ?? "Inget namn"}
         info={{ title: "användarnamn", description: "Byt ditt användarnamn." }}
+        onSubmit={onSubmit}
       />
       <DeleteUser id={id} />
     </div>
