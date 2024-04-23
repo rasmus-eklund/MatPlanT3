d4e0084fe51bad9e9ae7c02a83fe4a49686b8e52
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-23 12:50:09 +0200

    Redirect if user exist

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -1,13 +1,17 @@
 import { redirect } from "next/navigation";
 import { getServerAuthSession } from "~/server/auth";
 import RegisterUserForm from "./_components/RegisterUserForm";
+import { hasAccount } from "~/server/api/users";
 
 const RegisterUser = async () => {
   const user = await getServerAuthSession();
   if (!user) {
     redirect("/api/auth/login?");
   }
-
+  const hasAcc = await hasAccount(user.authId);
+  if (hasAcc) {
+    redirect("/");
+  }
   return (
     <div className="flex flex-col gap-5 bg-c3 p-10">
       <h1 className="rounded-md bg-c5 p-2 text-center text-lg text-c1">
