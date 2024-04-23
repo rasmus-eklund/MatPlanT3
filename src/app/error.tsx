3bda5fab6f03d17a7627c18db472544147d1b0bd
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-23 12:22:30 +0200

    Remove console log

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,7 +3,6 @@ import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { errorMessages } from "~/server/errors";
 
 const Error = ({ error }: { error: Error }) => {
-  console.log(error.message);
   if (error.message === errorMessages.UNAUTHORIZED) {
     return (
       <section className="flex h-1/3 flex-col items-center justify-center">
