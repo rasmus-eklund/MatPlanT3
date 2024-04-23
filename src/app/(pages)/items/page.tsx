7c100894c16776b34154ab70693bb5caa0fd908c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-23 12:22:49 +0200

    Add shopping list items page

--- src/app/(pages)/items/page.tsx
+++ src/app/(pages)/items/page.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import { addItem, getAllItems } from "~/server/api/items";
+import { getAllStores, getStoreBySlug } from "~/server/api/stores";
+import StoreSelect from "./_components/StoreSelect";
+import ApplyFilters from "./_components/ApplyFilters";
+import SearchItem from "~/components/common/SearchItem";
+export const dynamic = "force-dynamic";
 
-const page = async () => {
-  return <div>Items</div>;
+type Props = { searchParams?: { store?: string } };
+const page = async ({ searchParams }: Props) => {
+  const store = await getStoreBySlug(searchParams?.store);
+  const stores = await getAllStores();
+  const items = await getAllItems();
+
+  const itemsHome = items.filter((i) => i.home);
+  const itemsNotHome = items.filter((i) => !i.home);
+
+  return (
+    <div className="flex h-full flex-col gap-3 p-3">
+      <div className="flex justify-between">
+        <StoreSelect stores={stores} defaultStoreId={store.id} />
+        <SearchItem onSubmit={addItem} />
+      </div>
+      <ul className="flex flex-col gap-1 rounded-md bg-c5 p-1">
+        {store.store_categories.map((category) => (
+          <ApplyFilters
+            key={category.id + "_notHome"}
+            category={category}
+            items={itemsNotHome}
+          />
+        ))}
+        {items.length === 0 && (
+          <li className="rounded-md bg-c3 px-2 py-1 text-c5">
+            Din shoppinglista är tom.
+          </li>
+        )}
+      </ul>
+      {itemsHome.length !== 0 && (
+        <>
+          <h2>Hemma</h2>
+          {store.store_categories.map((category) => (
+            <ApplyFilters
+              key={category.id + "_Home"}
+              category={category}
+              items={itemsHome}
+            />
+          ))}
+        </>
+      )}
+    </div>
+  );
 };
 
 export default page;
