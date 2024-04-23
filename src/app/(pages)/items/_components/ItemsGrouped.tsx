b8a8e0a794cf457e40066c554dc0a01eefaa0fc8
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-23 12:24:32 +0200

    Grouped item

---
+++ src/app/(pages)/items/_components/ItemsGrouped.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import Icon from "~/icons/Icon";
+import ItemComponent from "./Item";
+import { capitalize } from "~/lib/utils";
+import { groupByUnit } from "./utils";
+import type { ItemsGrouped } from "~/types";
+import { checkItems } from "~/server/api/items";
+
+type Props = { group: ItemsGrouped };
+const ItemsGroupedComponent = ({ group: { name, checked, group } }: Props) => {
+  const [animate, setAnimate] = useState(checked);
+  const [open, setOpen] = useState(false);
+  if (group.length === 1 && group[0]) {
+    return <ItemComponent item={group[0]} />;
+  }
+  const onCheck = async () => {
+    setAnimate(!checked);
+    await checkItems({
+      ids: group.map(({ id }) => id),
+      checked: !checked,
+    });
+  };
+  return (
+    <li
+      className={`flex flex-col gap-1 rounded-md bg-c5 transition-opacity duration-200 ${
+        animate && "opacity-50"
+      }`}
+      key={name}
+    >
+      <div className="flex items-center gap-2 rounded-md bg-c3 px-2 py-1">
+        <input
+          className="cursor-pointer"
+          type="checkbox"
+          name="checkGroup"
+          checked={animate}
+          id={`check-group-${name}`}
+          onChange={onCheck}
+        />
+        <p className="grow font-bold text-c5">{capitalize(name)}</p>
+        <ul className="flex gap-1">
+          {groupByUnit(group).map((i, index, arr) => (
+            <li className="flex select-none gap-1 text-c5" key={i.unit}>
+              <p>{i.quantity}</p>
+              <p>{i.unit}</p>
+              {index < arr.length - 1 && <span>, </span>}
+            </li>
+          ))}
+        </ul>
+        <button className="cursor-pointer" onClick={() => setOpen(!open)}>
+          <Icon className="size-5 fill-c5" icon={open ? "up" : "down"} />
+        </button>
+      </div>
+      {open && (
+        <ul className="flex flex-col gap-1 rounded-b-md pl-4">
+          {group.map((item) => (
+            <ItemComponent key={item.id} item={item} />
+          ))}
+        </ul>
+      )}
+    </li>
+  );
+};
+
+export default ItemsGroupedComponent;
