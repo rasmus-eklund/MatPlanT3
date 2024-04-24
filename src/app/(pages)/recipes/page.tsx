a35db63b10938f20bc0f9d413441674438a6d26b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-24 07:52:48 +0200

    add search recipe form

--- src/app/(pages)/recipes/page.tsx
+++ src/app/(pages)/recipes/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import SearchRecipeForm from "./_components/SearchRecipe";
 
 type Props = {
-  searchParams?: { search?: string; page?: number; shared: boolean };
+  searchParams?: { search?: string; page?: number; shared?: "true" | "false" };
 };
 
 const page = ({ searchParams }: Props) => {
-  return <div>recipes</div>;
+  return (
+    <div className="flex flex-col gap-2 p-2">
+      <SearchRecipeForm />
+      recipes
+    </div>
+  );
 };
 
 export default page;
