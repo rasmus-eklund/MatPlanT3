d703ddb736e68256a3362c6cc99e095a9d9df550
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-27 07:59:40 +0200

    add pagination nav

---
+++ src/app/(pages)/recipes/_components/PaginationNav.tsx
@@ -0,0 +1,39 @@
+"use client";
+import Icon from "~/icons/Icon";
+import { useRouter } from "next/navigation";
+import type { SearchRecipeParams } from "~/types";
+import { formatUrl } from "./utils";
+
+type Props = { results: number; params: SearchRecipeParams };
+
+const PaginationNav = ({
+  results,
+  params: { page, search, shared },
+}: Props) => {
+  const router = useRouter();
+  return (
+    <div className="flex items-center justify-between">
+      <button
+        disabled={page === 1}
+        onClick={() => {
+          router.push(formatUrl({ search, shared, page: page - 1 }));
+        }}
+        className="disabled:opacity-20"
+      >
+        <Icon icon="caretLeft" className="h-10" />
+      </button>
+      <p>Sida: {page}</p>
+      <button
+        disabled={results < 10}
+        className="disabled:opacity-20"
+        onClick={() => {
+          router.push(formatUrl({ search, shared, page: page + 1 }));
+        }}
+      >
+        <Icon icon="caretRight" className="h-10" />
+      </button>
+    </div>
+  );
+};
+
+export default PaginationNav;
