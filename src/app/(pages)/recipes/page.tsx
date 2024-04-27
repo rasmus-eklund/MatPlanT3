b389ad355ab06c7a3745247accd866fd857b749f
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-27 08:00:34 +0200

    show found recipes

--- src/app/(pages)/recipes/page.tsx
+++ src/app/(pages)/recipes/page.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import SearchRecipeForm from "./_components/SearchRecipe";
+import FoundRecipes from "./_components/FoundRecipes";
 
 type Props = {
   searchParams?: { search?: string; page?: number; shared?: "true" | "false" };
@@ -9,7 +9,13 @@ const page = ({ searchParams }: Props) => {
   return (
     <div className="flex flex-col gap-2 p-2">
       <SearchRecipeForm />
-      recipes
+      <FoundRecipes
+        params={{
+          page: searchParams?.page ?? 0,
+          search: searchParams?.search ?? "",
+          shared: searchParams?.shared === "true" ?? false,
+        }}
+      />
     </div>
   );
 };
