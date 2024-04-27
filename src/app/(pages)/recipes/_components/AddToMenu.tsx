e3876c903e5e5fa47b2b718884095328a514d196
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-27 08:00:08 +0200

    add recipes button

---
+++ src/app/(pages)/recipes/_components/AddToMenu.tsx
@@ -0,0 +1,19 @@
+import ServerFormSubmit from "~/components/common/ServerFormSubmit";
+import { addToMenu } from "~/server/api/recipes";
+
+type Props = { id: string };
+
+const AddToMenu = ({ id }: Props) => {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await addToMenu(id);
+      }}
+    >
+      <ServerFormSubmit>Lägg till meny</ServerFormSubmit>
+    </form>
+  );
+};
+
+export default AddToMenu;
