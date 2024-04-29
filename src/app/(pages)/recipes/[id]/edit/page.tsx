bcd7844ae3fdc3bb70b44879bef01684b2928e1f
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 16:32:51 +0200

    add edit page

--- src/app/(pages)/recipes/[id]/edit/page.tsx
+++ src/app/(pages)/recipes/[id]/edit/page.tsx
@@ -1,6 +1,18 @@
+import { getRecipeById } from "~/server/api/recipes";
+import RecipeForm from "../../_components/RecipeForm";
+
 type Props = { params: { id: string } };
-const page = ({ params: { id } }: Props) => {
-  return <div>edit recipe id: {id}</div>;
+const page = async ({ params: { id } }: Props) => {
+  const recipe = await getRecipeById(id);
+  return (
+    <RecipeForm
+      recipe={recipe}
+      onSubmit={async (recipe) => {
+        "use server";
+        console.log(recipe);
+      }}
+    />
+  );
 };
 
 export default page;
