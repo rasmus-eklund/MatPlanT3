fd81669addc2913f87873f05fdf036df41fcc1c0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 15:47:15 +0200

    add crud factory

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+import type { Dispatch, SetStateAction } from "react";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -22,3 +23,27 @@ export const slugify = (str: string) => {
 
 export const delay = async (time: number) =>
   new Promise((resolve) => setTimeout(resolve, time));
+
+const crudFactory = <T extends { id: string }>(
+  fn: Dispatch<SetStateAction<T[]>>,
+) => {
+  const add = (item: T) => {
+    fn((p) => [...p, item]);
+  };
+  const remove = ({ id }: { id: string }) => {
+    fn((p) => p.filter((i) => i.id !== id));
+  };
+  const update = (item: T) => {
+    fn((p) => {
+      const index = p.findIndex((i) => i.id === item.id);
+      const newItems = [...p];
+      if (index !== -1) {
+        newItems[index] = item;
+      }
+      return newItems;
+    });
+  };
+  return { add, remove, update };
+};
+
+export default crudFactory;
