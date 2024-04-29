d051dd41f70e8032a8e81dee3c87e9535ba6d991
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 16:32:32 +0200

    add animation

--- src/icons/Icon.tsx
+++ src/icons/Icon.tsx
@@ -13,7 +13,10 @@ const Icon = ({ icon, className, ...rest }: Props) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      className={cn("size-6", className)}
+      className={cn(
+        "size-6 transition-all duration-200 md:hover:scale-105",
+        className,
+      )}
       {...rest}
     >
       <path d={svgPaths[icon]} />
