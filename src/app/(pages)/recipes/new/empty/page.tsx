5330189d69b26ea391f927ca875857eff8ccfc44
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 15:46:17 +0200

    add create recipe page

---
+++ src/app/(pages)/recipes/new/empty/page.tsx
@@ -0,0 +1,21 @@
+import RecipeForm from "../../_components/RecipeForm";
+import { createRecipe } from "~/server/api/recipes";
+import { type Recipe } from "~/server/shared";
+
+const page = () => {
+  const recipe: Recipe = {
+    id: "placeholder",
+    ingredients: [],
+    contained: [],
+    instruction: "Instruktion",
+    isPublic: false,
+    name: "Nytt recept",
+    quantity: 2,
+    unit: "port",
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  return <RecipeForm recipe={recipe} onSubmit={createRecipe} />;
+};
+
+export default page;
