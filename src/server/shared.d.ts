cc9ad1f3523aefd5b61e052f4cfc4fd067b90d22
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 15:47:24 +0200

    add recipe

--- src/server/shared.d.ts
+++ src/server/shared.d.ts
@@ -1,5 +1,6 @@
 import type { getAllIngredients, getAllCategories } from "./api/admin";
 import type { getAllItems } from "./api/items";
+import type { getRecipeById } from "./api/recipes";
 import type { getAllStores, getStoreById } from "./api/stores";
 import type { getUserStats, getAllUsers } from "./api/users";
 import type { getServerAuthSession } from "./auth";
@@ -18,4 +19,6 @@ export type Store = Awaited<ReturnType<typeof getAllStores>>;
 
 export type StoreWithItems = Awaited<ReturnType<typeof getStoreById>>;
 
-export type Item = (Awaited<ReturnType<typeof getAllItems>>)[number];
+export type Item = Awaited<ReturnType<typeof getAllItems>>[number];
+
+export type Recipe = Awaited<ReturnType<typeof getRecipeById>>;
