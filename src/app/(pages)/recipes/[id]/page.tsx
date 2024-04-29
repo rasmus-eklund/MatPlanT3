9339438bfad3192c92638c848ec8e1fd7789998a
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 16:07:56 +0200

    add recipe view

--- src/app/(pages)/recipes/[id]/page.tsx
+++ src/app/(pages)/recipes/[id]/page.tsx
@@ -1,6 +1,15 @@
+import { getRecipeById } from "~/server/api/recipes";
+import RecipeComponent from "./_components/recipe";
+
 type Props = { params: { id: string } };
-const page = ({ params: { id } }: Props) => {
-  return <div>recipe id: {id}</div>;
+
+const page = async ({ params: { id } }: Props) => {
+  const recipe = await getRecipeById(id);
+  return (
+    <div className="flex flex-col gap-5">
+      <RecipeComponent recipe={recipe} />
+    </div>
+  );
 };
 
 export default page;
