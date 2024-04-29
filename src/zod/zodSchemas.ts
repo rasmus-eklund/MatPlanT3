26a890e7ace6eb97bf9cb2e90b46e264f5987753
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 15:48:39 +0200

    remove unused

--- src/zod/zodSchemas.ts
+++ src/zod/zodSchemas.ts
@@ -1,74 +1,20 @@
 import units from "~/lib/constants/units";
 import { z } from "zod";
 
-export const zId = z.object({ id: z.string().min(1) });
-export type tId = z.infer<typeof zId>;
-
-export const zNameId = z.object({
-  name: z.string().min(1, "Name should be minimum 1 character."),
-  id: z.string().min(1),
-});
-
-export const zName = z.object({ name: z.string().min(2) });
-export type tName = z.infer<typeof zName>;
-
-export const zStoreOrder = z.array(
-  z.object({
-    categoryId: z.coerce.number(),
-    subcategoryId: z.coerce.number(),
-  }),
-);
-export type tStoreOrder = z.infer<typeof zStoreOrder>;
-
-export const zRecipe = z.object({
-  id: z.string().min(1),
+export const recipeSchema = z.object({
   name: z.string().min(2),
   quantity: z.coerce.number().positive(),
   unit: z.enum(units),
   instruction: z.string(),
   isPublic: z.boolean(),
 });
-export type tRecipe = z.infer<typeof zRecipe>;
-
-export const zIngredient = z.object({
-  id: z.string().min(1),
-  ingredientId: z.string().min(1),
-  quantity: z.coerce.number().positive(),
-  unit: z.enum(units),
-  name: z.string().min(1),
-});
-export type tIngredient = z.infer<typeof zIngredient>;
-
-export const zContained = z.object({
-  id: z.string().min(1),
-  containedRecipeId: z.string().min(1),
-  name: z.string().min(1),
-  quantity: z.number().positive(),
-});
-export type tContained = z.infer<typeof zContained>;
+export type RecipeType = z.infer<typeof recipeSchema>;
 
 export const searchRecipeSchema = z.object({
   search: z.string(),
   page: z.coerce.number().positive(),
-  shared: z.enum(["true", "false"]),
+  shared: z.boolean(),
 });
-export type SearchRecipe = z.infer<typeof searchRecipeSchema>;
-
-export const zFullRecipe = z.object({
-  recipe: zRecipe,
-  ingredients: z.array(
-    z.object({
-      id: z.string().min(1),
-      ingredientId: z.string().min(1),
-      quantity: z.coerce.number().positive(),
-      unit: z.enum(units),
-      name: z.string().min(1),
-      order: z.number(),
-    }),
-  ),
-  contained: z.array(zContained),
-});
-export type tFullRecipe = z.infer<typeof zFullRecipe>;
 
 export const zIngredientCat = z.object({
   name: z.string().min(1),
@@ -104,5 +50,6 @@ export const itemSchema = z.object({
   quantity: z.coerce.number().positive(),
   unit: z.enum(units),
   ingredientId: z.string().uuid(),
+  name: z.string(),
 });
 export type Item = z.infer<typeof itemSchema>;
