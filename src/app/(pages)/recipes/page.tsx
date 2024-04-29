a7ce0d648a2f32073f1cdbb61f0cd312e759b42c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-29 15:32:20 +0200

    page should be 1

--- src/app/(pages)/recipes/page.tsx
+++ src/app/(pages)/recipes/page.tsx
@@ -11,7 +11,7 @@ const page = ({ searchParams }: Props) => {
       <SearchRecipeForm />
       <FoundRecipes
         params={{
-          page: searchParams?.page ?? 0,
+          page: searchParams?.page ?? 1,
           search: searchParams?.search ?? "",
           shared: searchParams?.shared === "true" ?? false,
         }}
