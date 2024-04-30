fbc1831cbf9417a1a2ae5d58393614bafacb8e58
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 11:53:53 +0200

    remove shared count

--- src/app/(pages)/user/_components/UserStats.tsx
+++ src/app/(pages)/user/_components/UserStats.tsx
@@ -1,26 +1,13 @@
 import Icon from "~/icons/Icon";
 import type { UserStats } from "~/server/shared";
 
-const UserStats = async ({
-  recipe,
-  shared,
-  menu,
-  item,
-  store,
-}: UserStats["count"]) => {
+const UserStats = async ({ recipe, menu, item, store }: UserStats["count"]) => {
   return (
     <div className="bg-c3">
       <ul className="flex flex-col gap-2 p-1">
         <li className="flex items-center gap-2">
           <Icon icon="recipes" className="size-10 fill-c5" />
           <span>Recept: {recipe}</span>
-        </li>
-        <li className="flex items-center gap-2">
-          <Icon
-            icon="recipes"
-            className="size-10 rounded-full border border-c5 fill-c5"
-          />
-          <span>Delade: {shared}</span>
         </li>
         <li className="flex items-center gap-2">
           <Icon icon="home" className="size-10 fill-c5" />
