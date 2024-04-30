d7018388b110f2b35eb57218f3d4ac9266d0dd2d
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 06:47:41 +0200

    add delete button

---
+++ src/components/common/DeleteButton.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useFormStatus } from "react-dom";
+import { ClipLoader } from "react-spinners";
+import { Button } from "~/components/ui/button";
+import Icon from "~/icons/Icon";
+
+type Props = { icon?: boolean };
+const DeleteButton = ({ icon = true }: Props) => {
+  const { pending } = useFormStatus();
+  if (pending) {
+    return <ClipLoader size={18} />;
+  } else {
+    if (icon) {
+      return (
+        <button>
+          <Icon
+            icon="delete"
+            className="size-5 fill-black hover:scale-110 hover:fill-red-600"
+          />
+        </button>
+      );
+    }
+    return <Button variant={"destructive"}>Ta bort</Button>;
+  }
+};
+
+export default DeleteButton;
