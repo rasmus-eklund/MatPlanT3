3518683f62026110984005c966e5eff20f3cd338
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 09:28:26 +0200

    add date picker

---
+++ src/components/common/DatePicker.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Calendar as CalendarIcon } from "lucide-react";
+import { useState } from "react";
+
+import { Button } from "~/components/ui/button";
+import { Calendar } from "~/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "~/components/ui/popover";
+import { cn } from "~/lib/utils";
+
+const DatePicker = () => {
+  const [date, setDate] = useState<Date>();
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-fit justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? (
+            date.toLocaleDateString("sv-SE", {
+              dateStyle: "short",
+            })
+          ) : (
+            <span>Välj datum</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default DatePicker;
