d9c0aa28bffca18b886e8c47630713674b3f5f6b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 11:54:35 +0200

    add delete recipe dialog

--- src/app/(pages)/recipes/[id]/page.tsx
+++ src/app/(pages)/recipes/[id]/page.tsx
@@ -2,29 +2,60 @@ import { getRecipeById, removeRecipe } from "~/server/api/recipes";
 import RecipeView from "~/components/common/RecipeView";
 import DeleteDialog from "~/components/common/DeleteDialog";
 import DeleteButton from "~/components/common/DeleteButton";
+import Link from "next/link";
+import { capitalize } from "~/lib/utils";
+import { unitsAbbr } from "~/lib/constants/units";
+import { type Recipe } from "~/server/shared";
+import BackButton from "~/components/common/BackButton";
 
-type Props = { params: { id: string } };
+type Props = { params: { id: string }; searchParams?: { from?: string } };
 
 const page = async ({ params: { id } }: Props) => {
   const recipe = await getRecipeById(id);
   return (
     <div className="flex flex-col gap-5">
       <RecipeView recipe={recipe}>
-        <div>
-          <DeleteDialog info={{ title: "recept" }}>
-            <form
-              action={async () => {
-                "use server";
-                await removeRecipe(id);
-              }}
-            >
-              <DeleteButton icon={false} />
-            </form>
-          </DeleteDialog>
+        <ContainedRecipes contained={recipe.contained} unit={recipe.unit} />
+        <div className="flex justify-between">
+          <BackButton />
+          {recipe.yours && (
+            <DeleteDialog info={{ title: "recept" }}>
+              <form
+                action={async () => {
+                  "use server";
+                  await removeRecipe(id);
+                }}
+              >
+                <DeleteButton icon={false} />
+              </form>
+            </DeleteDialog>
+          )}
         </div>
       </RecipeView>
     </div>
   );
 };
 
+type ContainedProps = { contained: Recipe["contained"]; unit: Recipe["unit"] };
+const ContainedRecipes = ({ contained, unit }: ContainedProps) => {
+  if (!!contained.length)
+    return (
+      <>
+        <h2 className="text-lg text-c5">Kopplade recept</h2>
+        <ul className="rounded-md bg-c4 p-1">
+          {contained.map(({ id, name, quantity, recipeId }) => (
+            <li
+              className="flex items-center justify-between rounded-md bg-c2 p-2"
+              key={id}
+            >
+              <Link href={`/recipes/${recipeId}`}>{capitalize(name)}</Link>
+              <span>
+                {quantity} {unitsAbbr[unit]}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+};
 export default page;
