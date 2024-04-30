216fcb3a0d8fdda06abec11b825a75250807b9c1
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 11:54:55 +0200

    Feat: change day and quantity

--- src/app/(pages)/menu/_components/MenuItem.tsx
+++ src/app/(pages)/menu/_components/MenuItem.tsx
@@ -1,31 +1,46 @@
 import Link from "next/link";
 import DatePicker from "~/components/common/DatePicker";
 import DeleteButton from "~/components/common/DeleteButton";
-import { removeMenuItem } from "~/server/api/menu";
-// import {  useState } from "react";
+import { unitsAbbr } from "~/lib/constants/units";
+import { updateMenuDate, removeMenuItem } from "~/server/api/menu";
 import { type MenuItem } from "~/server/shared";
+import EditQuantity from "./EditQuantity";
 
 type Props = {
   item: MenuItem;
 };
 
-const MenuItemComponent = ({ item }: Props) => {
-  // const [portions, setPortions] = useState(item.quantity);
-  // const debouncedPortions = useDebounce(portions, 1000);
-  const {
+const MenuItemComponent = ({
+  item: {
     id,
-    recipe: { name },
-  } = item;
-
+    quantity,
+    day,
+    recipe: { name, unit },
+  },
+}: Props) => {
   return (
     <li className="flex flex-col gap-2 rounded-md bg-c2 px-2 font-bold text-c5">
       <div>
         <Link href={`/menu/${id}`}>{name}</Link>
       </div>
-      <div className="flex w-full select-none items-center gap-2 py-2 md:justify-between">
-        <DatePicker />
-        <div className="flex items-center gap-1">Ändra kvantitet</div>
+      <div className="flex w-full select-none items-center justify-between gap-2 py-2">
+        <div className="flex items-center gap-2">
+          <DatePicker
+            date={day ? new Date(day) : undefined}
+            setDate={async (date) => {
+              "use server";
+              await updateMenuDate(id, date);
+            }}
+          />
+          <div className="flex items-center gap-2">
+            <span>
+              {quantity} {unitsAbbr[unit]}
+            </span>
+            <EditQuantity id={id} quantity={quantity} />
+          </div>
+        </div>
         <form
+          className="flex items-center"
           action={async () => {
             "use server";
             await removeMenuItem(id);
