e8ae628e2f45e00dad9a61ae86d5e1574f2a4ea3
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 06:47:30 +0200

    Feat: delete recipe

--- src/app/(pages)/recipes/[id]/page.tsx
+++ src/app/(pages)/recipes/[id]/page.tsx
@@ -1,5 +1,7 @@
-import { getRecipeById } from "~/server/api/recipes";
+import { getRecipeById, removeRecipe } from "~/server/api/recipes";
 import RecipeView from "~/components/common/RecipeView";
+import DeleteDialog from "~/components/common/DeleteDialog";
+import DeleteButton from "~/components/common/DeleteButton";
 
 type Props = { params: { id: string } };
 
@@ -7,7 +9,20 @@ const page = async ({ params: { id } }: Props) => {
   const recipe = await getRecipeById(id);
   return (
     <div className="flex flex-col gap-5">
-      <RecipeView recipe={recipe} />
+      <RecipeView recipe={recipe}>
+        <div>
+          <DeleteDialog info={{ title: "recept" }}>
+            <form
+              action={async () => {
+                "use server";
+                await removeRecipe(id);
+              }}
+            >
+              <DeleteButton icon={false} />
+            </form>
+          </DeleteDialog>
+        </div>
+      </RecipeView>
     </div>
   );
 };
