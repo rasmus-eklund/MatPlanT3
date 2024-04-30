630f18de4099858cefdd7b99e4e7daef71bb1363
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 09:31:13 +0200

    add menu page

---
+++ src/app/(pages)/menu/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Icon from "~/icons/Icon";
+import { getMenu } from "~/server/api/menu";
+import MenuItemComponent from "./_components/MenuItem";
+export const dynamic = "force-dynamic";
+
+const page = async () => {
+  const items = await getMenu();
+  if (items.length === 0) {
+    return (
+      <div className="flex flex-col items-center gap-1 self-center pt-20">
+        <p className="text-xl text-c2">Här var det tomt.</p>
+        <p className="text-center">
+          Klicka på besticken för att lägga till ett recept:
+        </p>
+        <Link href="/recipes">
+          <Icon className="size-10 fill-c3" icon="recipes" />
+        </Link>
+      </div>
+    );
+  }
+  return (
+    <ul className="space-y-4 p-4">
+      {items.map((item) => (
+        <MenuItemComponent key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+};
+
+export default page;
