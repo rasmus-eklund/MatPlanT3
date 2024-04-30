293431a7d7571088e63e71ad9e87ed6d867b1ab7
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-04-30 11:52:33 +0200

    change date and quantity

--- src/server/api/menu.ts
+++ src/server/api/menu.ts
@@ -1,4 +1,11 @@
-import { ensureError, getAllContained } from "~/lib/utils";
+"use server";
+
+import {
+  dateToString,
+  ensureError,
+  getAllContained,
+  scaleIngredients,
+} from "~/lib/utils";
 import { authorize } from "../auth";
 import { getRecipeById } from "./recipes";
 import { db } from "../db";
@@ -7,12 +14,13 @@ import { randomUUID } from "crypto";
 import { errorMessages } from "../errors";
 import { and, eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
+import { notFound } from "next/navigation";
 
 export const getMenu = async () => {
   const user = await authorize();
   return await db.query.menu.findMany({
     where: (m, { eq }) => eq(m.userId, user.id),
-    with: { recipe: { columns: { name: true } } },
+    with: { recipe: { columns: { name: true, unit: true } } },
   });
 };
 
@@ -61,4 +69,41 @@ export const removeMenuItem = async (id: string) => {
   const user = await authorize();
   await db.delete(menu).where(and(eq(menu.id, id), eq(menu.userId, user.id)));
   revalidatePath("/menu");
+};
+
+export const updateMenuDate = async (id: string, date: Date) => {
+  const user = await authorize();
+  await db
+    .update(menu)
+    .set({ day: dateToString(date) })
+    .where(and(eq(menu.id, id), eq(menu.userId, user.id)));
+  revalidatePath("/menu");
+};
+
+export const updateMenuQuantity = async (id: string, quantity: number) => {
+  const user = await authorize();
+  const res = await db.query.menu.findFirst({
+    where: (m, { eq, and }) => and(eq(m.id, id), eq(m.userId, user.id)),
+    with: { items: true },
+  });
+
+  if (!res) {
+    notFound();
+  }
+  const scale = quantity / res.quantity;
+  const scaled = scaleIngredients(res.items, scale);
+  await db.transaction(async (tx) => {
+    await tx
+      .update(menu)
+      .set({ quantity })
+      .where(and(eq(menu.id, id), eq(menu.userId, user.id)));
+    for (const ing of scaled) {
+      await tx
+        .update(items)
+        .set({ quantity: ing.quantity })
+        .where(and(eq(items.id, ing.id), eq(items.userId, user.id)));
+    }
+  });
+  revalidatePath("/menu");
+  revalidatePath("/items");
 };
