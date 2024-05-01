495a44a9e13d69b368d985e75dcbc0a06ca80cce
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-01 07:54:35 +0200

    move to utils

--- src/app/(pages)/recipes/_components/utils.ts
+++
@@ -1,5 +0,0 @@
-import type { SearchRecipeParams } from "~/types";
-
-export const formatUrl = ({ search, shared, page }: SearchRecipeParams) => {
-  return `/recipes?search=${search}&page=${page}&shared=${shared ? "true" : "false"}`;
-};
