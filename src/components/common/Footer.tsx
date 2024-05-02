06d83935b907ee4e1d6175c79dfd445b51263008
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-02 16:19:46 +0200

    remove about

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,13 +1,8 @@
-import Link from "next/link";
-
 const Footer = () => {
   return (
     <footer className="flex min-h-[25px] w-full max-w-5xl justify-between bg-c5 px-2 text-c4">
       <p>&copy; Rasmus Eklund</p>
       <div className="flex items-center gap-4">
-        <Link className="hover:text-c2" href={"/about"}>
-          About
-        </Link>
         <a
           className="hover:text-c2"
           href="https://github.com/rasmus-eklund"
