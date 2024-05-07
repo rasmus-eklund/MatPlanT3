bed4aae95ad6fc081efffe11310174f9ee599cbe
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-07 12:21:03 +0200

    Refactor: handle add new recipe

--- src/app/(pages)/recipes/new/empty/page.tsx
+++ src/app/(pages)/recipes/new/empty/page.tsx
@@ -1,11 +1,15 @@
+import { randomUUID } from "crypto";
 import RecipeForm from "../../_components/RecipeForm";
 import { createRecipe } from "~/server/api/recipes";
-import { type Recipe } from "~/server/shared";
+import type { Recipe } from "~/server/shared";
+import type { RecipeFormSubmit } from "~/types";
+import { extractGroups } from "~/lib/utils";
 
 const page = () => {
+  const recipeId = randomUUID();
   const recipe: Recipe = {
     yours: true,
-    id: "placeholder",
+    id: recipeId,
     ingredients: [],
     contained: [],
     instruction: "Instruktion",
@@ -15,8 +19,17 @@ const page = () => {
     unit: "port",
     createdAt: new Date(),
     updatedAt: new Date(),
+    groups: [],
   };
-  return <RecipeForm recipe={recipe} onSubmit={createRecipe} />;
+  const handleSubmit = async (data: RecipeFormSubmit) => {
+    "use server";
+    const { groups, ingredients } = extractGroups({
+      groups: data.groups,
+      recipeId: data.id,
+    });
+    await createRecipe({ ...data, groups, ingredients });
+  };
+  return <RecipeForm recipe={recipe} onSubmit={handleSubmit} />;
 };
 
 export default page;
