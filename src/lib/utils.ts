0204c31001993d22b1cfe91a22d7d5d430ab75a0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-07 12:21:40 +0200

    Refactor: collect scattered utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,11 @@ import { twMerge } from "tailwind-merge";
 import type { Item, Recipe } from "~/server/shared";
 import { getRecipeById } from "~/server/api/recipes";
 import { errorMessages } from "~/server/errors";
-import { type SearchRecipeParams } from "~/types";
+import type {
+  CreateRecipeInput,
+  IngredientGroup,
+  SearchRecipeParams,
+} from "~/types";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
 
@@ -116,29 +120,29 @@ export const dateToString = (date: Date) =>
     dateStyle: "short",
   });
 
-export const formatDate = (date: Date): string => {
-  const weekdays = [
-    "Söndag",
-    "Måndag",
-    "Tisdag",
-    "Onsdag",
-    "Torsdag",
-    "Fredag",
-    "Lördag",
-  ];
-  const weekday = weekdays[date.getDay()];
-  return `${weekday}, ${date.getDate()}/${date.getMonth() + 1}`;
-};
-
 export const create_copy = (recipeId: string, recipe: Recipe) => {
-  const { ingredients, instruction, name, quantity, unit } = recipe;
-  const newIngredients = ingredients.map(({ id: _, ...i }) => ({
-    ...i,
+  const { ingredients, instruction, name, quantity, unit, groups } = recipe;
+  const newGroups = groups.map(({ name, order }) => ({
+    id: crypto.randomUUID(),
+    name,
+    order,
     recipeId,
   }));
+  const newIngredients = ingredients.map(({ id: _, group, ...i }) => {
+    const found = newGroups.find((i) => i.name === group?.name);
+    if (!found) {
+      throw new Error("Group not found");
+    }
+    return {
+      ...i,
+      recipeId,
+      groupId: found.id,
+    };
+  });
   return {
     newRecipe: { name, quantity, unit, instruction, id: recipeId },
     newIngredients,
+    newGroups,
   };
 };
 
@@ -159,4 +163,91 @@ export const sortItemsByHomeAndChecked = (items: Item[]) => {
     }
   }
   return sorted;
+};
+
+export const extractGroups = (data: {
+  groups: IngredientGroup[];
+  recipeId: string;
+}) => {
+  const groups: CreateRecipeInput["groups"] = [];
+  const ingredients: CreateRecipeInput["ingredients"] = [];
+  data.groups.forEach((group, order) => {
+    const groupId = group.id === "recept" ? crypto.randomUUID() : group.id;
+    groups.push({
+      name: group.name,
+      order,
+      id: groupId,
+      recipeId: data.recipeId,
+    });
+    ingredients.push(...group.ingredients.map((i) => ({ ...i, groupId })));
+  });
+  return { groups, ingredients };
+};
+
+export const findArrayDifferences = <Item extends { id: string }>(
+  A: Item[],
+  B: Item[],
+) => {
+  const edited: Item[] = [];
+  const added: Item[] = [];
+  const removed: string[] = [];
+  const mapA = new Map(A.map((item) => [item.id, item]));
+
+  for (const itemB of B) {
+    if (mapA.has(itemB.id)) {
+      const itemA = mapA.get(itemB.id)!;
+      if (!isEqual(itemA, itemB)) {
+        edited.push(itemB);
+      }
+      mapA.delete(itemB.id);
+    } else {
+      added.push(itemB);
+    }
+  }
+
+  for (const itemA of mapA.values()) {
+    removed.push(itemA.id);
+  }
+
+  return { edited, added, removed };
+};
+
+const isEqual = <T extends { id: string }>(a: T, b: T) => {
+  for (const key of Object.keys(a)) {
+    if (key !== "id" && a[key as keyof T] !== b[key as keyof T]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const groupIngredients = (ingredients: Recipe["ingredients"]) => {
+  const groups: IngredientGroup[] = [];
+  for (const ing of ingredients) {
+    if (!ing.group) {
+      const group = groups.find((i) => i.id === "recept");
+      if (!group) {
+        groups.push({
+          id: "recept",
+          name: "recept",
+          order: 0,
+          ingredients: [ing],
+        });
+      } else {
+        group.ingredients.push(ing);
+      }
+    } else {
+      const { name, id, order } = ing.group;
+      const group = groups.find((group) => group.name === name);
+      if (!group) {
+        groups.push({ id, name, ingredients: [ing], order });
+      } else {
+        group.ingredients.push(ing);
+      }
+    }
+  }
+  for (const group of groups) {
+    group.ingredients.sort((a, b) => a.order - b.order);
+  }
+  return groups.sort((a, b) => a.order - b.order);
 };
