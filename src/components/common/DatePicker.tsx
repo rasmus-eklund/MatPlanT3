475c136f3e7c8d377af09386497887889a90b6bf
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-05-12 06:52:13 +0200

    Refactor: update loading and error states

--- src/components/common/DatePicker.tsx
+++ src/components/common/DatePicker.tsx
@@ -2,6 +2,8 @@
 
 import { Calendar as CalendarIcon } from "lucide-react";
 import { useState } from "react";
+import { ClipLoader } from "react-spinners";
+import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
 import { Calendar } from "~/components/ui/calendar";
 import {
@@ -17,23 +19,36 @@ type Props = {
 };
 const DatePicker = ({ date, setDate }: Props) => {
   const [open, setOpen] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const handleChange = async (newDate: Date | undefined) => {
     if (newDate) {
-      await setDate(dateToString(newDate));
+      setIsSubmitting(true);
+      setOpen(false);
+      try {
+        await setDate(dateToString(newDate));
+        toast.success("Datum ändrat!");
+      } catch (error) {
+        toast.error("Något gick fel...");
+      }
+      setIsSubmitting(false);
     }
-    setOpen(false);
   };
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <PopoverTrigger asChild>
         <Button
+          disabled={isSubmitting}
           variant={"outline"}
           className={cn(
             "w-fit justify-start text-left font-normal",
             !date && "text-muted-foreground",
           )}
         >
-          <CalendarIcon className="mr-2 h-4 w-4" />
+          {isSubmitting ? (
+            <ClipLoader size={20} className="mr-2" />
+          ) : (
+            <CalendarIcon className="mr-2 size-4" />
+          )}
           {date ? dateToString(date) : <span>Välj datum</span>}
         </Button>
       </PopoverTrigger>
