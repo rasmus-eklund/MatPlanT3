606275784c0611bbd2e007c1f5296c2bd79ecc0e
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2024-11-22 16:22:52 +0100

    try fix some login issues

--- next.config.js
+++ next.config.js
@@ -7,6 +7,13 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   images: { remotePatterns: [{ hostname: "lh3.googleusercontent.com" }] },
+  env: {
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/user`
+  }
 };
 
 export default config;
