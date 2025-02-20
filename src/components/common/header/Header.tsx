fe8e683ab3389563654b301aeaf46e4d1541fd71
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-20 06:51:26 +0100

    make logo link

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -3,19 +3,22 @@ import NavLinks from "./NavLinks";
 import Image from "next/image";
 import { LoginLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { Button } from "~/components/ui/button";
+import Link from "next/link";
 
 const Header = async () => {
   const user = await getServerAuthSession();
   return (
     <header className="bg-c5 sticky top-0 z-10 flex w-full max-w-5xl items-center justify-between pr-2">
-      <Image
-        className={"w-28 md:w-52"}
-        priority={true}
-        src={"/logo-color.svg"}
-        alt="MatPlan logo"
-        width={478}
-        height={222}
-      />
+      <Link href="/">
+        <Image
+          className={"w-28 md:w-52"}
+          priority={true}
+          src={"/logo-color.svg"}
+          alt="MatPlan logo"
+          width={478}
+          height={222}
+        />
+      </Link>
       {user ? (
         <NavLinks user={user} />
       ) : (
