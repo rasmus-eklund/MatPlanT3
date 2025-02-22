068f6855f3c45500c41a52fcd02881b60ee0c16b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-22 12:06:27 +0100

    lint

--- src/app/(pages)/stores/page.tsx
+++ src/app/(pages)/stores/page.tsx
@@ -6,8 +6,8 @@ const Stores = async () => {
   const stores = await getAllStores();
   const deletable = stores.length !== 1;
   return (
-    <div className="flex flex-col gap-2 rounded-md bg-c3 p-3">
-      <h2 className="text-xl text-c5">Dina butiker</h2>
+    <div className="bg-c3 flex flex-col gap-2 rounded-md p-3">
+      <h2 className="text-c5 text-xl">Dina butiker</h2>
       <ul className="flex flex-col gap-2">
         {stores.map((store) => (
           <StoreItem key={store.id} store={store} deleteable={deletable} />
