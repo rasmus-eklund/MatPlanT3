5f43635a5755a3e5f973cfd7258ef83226345987
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-02-24 06:11:56 +0100

    add missing items

--- src/app/(pages)/recipes/new/link/page.tsx
+++ src/app/(pages)/recipes/new/link/page.tsx
@@ -18,6 +18,8 @@ import type { CreateRecipeInput, ExternalRecipe } from "~/types";
 import { ClipLoader } from "react-spinners";
 import EditItem from "~/components/common/EditItem";
 import { type Item } from "~/zod/zodSchemas";
+import { toast } from "sonner";
+import { createRecipe } from "~/server/api/recipes";
 
 const urlSchema = z.object({ url: z.string().url() });
 type UrlSchema = z.infer<typeof urlSchema>;
@@ -69,6 +71,49 @@ const GetByLink = () => {
       return { ...p, ingredients: newIngredients };
     });
   };
+
+  const addItem = async (ing: Item) => {
+    if (!recipe) return;
+    const match = {
+      ...ing,
+      recipeId: recipe.recipeId,
+      groupId: null,
+      order: 0,
+    };
+    setRecipe((p) => {
+      if (!p) return p;
+      const newIngredients = p.ingredients.map((i) => {
+        if (i.id === ing.id) {
+          return { ...i, match };
+        }
+        return i;
+      });
+      return { ...p, ingredients: newIngredients };
+    });
+  };
+
+  const saveRecipe = async () => {
+    if (!recipe) return;
+    setLoading(true);
+    const newRecipe: CreateRecipeInput = {
+      id: recipe.recipeId,
+      name: recipe.name,
+      quantity: recipe.quantity,
+      unit: recipe.unit,
+      instruction: recipe.instruction,
+      isPublic: false,
+      ingredients: recipe.ingredients.map((i, order) => ({
+        ...i.match,
+        order,
+      })),
+      groups: [],
+      contained: [],
+    };
+    await createRecipe(newRecipe);
+    setLoading(false);
+    toast.success("Lade till recept");
+  };
+
   return (
     <div className="bg-c3 flex flex-col gap-2 p-3">
       <Form {...form}>
@@ -96,7 +141,19 @@ const GetByLink = () => {
       </Form>
       {loading && <ClipLoader size={80} />}
       {recipe ? (
-        <Comparison recipe={recipe} updateItem={updateItem} />
+        <div>
+          <Comparison
+            recipe={recipe}
+            updateItem={updateItem}
+            addItem={addItem}
+          />
+          <div className="flex justify-end gap-2">
+            <Button disabled={loading} onClick={saveRecipe}>
+              Spara
+            </Button>
+            <Button onClick={() => setRecipe(null)}>Avbryt</Button>
+          </div>
+        </div>
       ) : (
         <div>
           <h2>Sidor som kan användas för att läsa recept</h2>
@@ -132,32 +189,39 @@ const links: { name: string; url: string }[] = [
 const Comparison = ({
   recipe,
   updateItem,
+  addItem,
 }: {
   recipe: ExternalRecipe;
   updateItem: (item: Item) => Promise<void>;
+  addItem: (item: Item) => Promise<void>;
 }) => {
   const { ingredients, instruction, name } = recipe;
 
   return (
-    <div className="bg-c3 flex flex-col gap-2 p-3">
+    <div className="bg-c3 flex flex-col gap-4 p-3">
       <h2 className="text-c5 text-2xl">{name}</h2>
       {ingredients.map(({ id, input, match }) => {
         return (
           <div className="flex gap-2" key={id}>
             <p>{input}</p>
             --&gt;
-            {match && (
+            {match.name ? (
               <div className="flex items-center gap-2">
-                <p>{match.name}</p>
                 <p>{match.quantity}</p>
                 <p>{match.unit}</p>
+                <p>{match.name}</p>
                 <EditItem item={match} onUpdate={updateItem} />
+              </div>
+            ) : (
+              <div className="flex items-center gap-2">
+                <p className="text-c1">Kunde inte matcha ingrediens</p>
+                <EditItem item={match} onUpdate={addItem} add />
               </div>
             )}
           </div>
         );
       })}
-      <h3>Instruktion</h3>
+      <h3 className="text-c5 text-lg">Instruktion</h3>
       <ol>
         {instruction.split("\n\n").map((i, n) => (
           <li key={`instruction-${n}`}>{i}</li>
