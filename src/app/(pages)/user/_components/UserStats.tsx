6a6501cc14c1204eec75c2c5b0da33adeb723b51
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-10-06 23:29:04 +0200

    smaller icons

--- src/app/(pages)/user/_components/UserStats.tsx
+++ src/app/(pages)/user/_components/UserStats.tsx
@@ -6,19 +6,19 @@ const UserStats = async ({ recipe, menu, item, store }: UserStats["count"]) => {
     <div className="bg-c3">
       <ul className="flex flex-col gap-2 p-1">
         <li className="flex items-center gap-2">
-          <Icon icon="Utensils" className="size-10" />
+          <Icon icon="Utensils" className="size-5" />
           <span>Recept: {recipe}</span>
         </li>
         <li className="flex items-center gap-2">
-          <Icon icon="MenuSquare" className="size-10" />
+          <Icon icon="MenuSquare" className="size-5" />
           <span>Meny: {menu}</span>
         </li>
         <li className="flex items-center gap-2">
-          <Icon icon="ShoppingCart" className="size-10" />
+          <Icon icon="ShoppingCart" className="size-5" />
           <span>Inköpslista: {item}</span>
         </li>
         <li className="flex items-center gap-2">
-          <Icon icon="Store" className="size-10" />
+          <Icon icon="Store" className="size-5" />
           <span>Affärer: {store}</span>
         </li>
       </ul>
