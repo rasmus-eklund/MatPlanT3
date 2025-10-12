9355acf137b9b0920d987aa47eab53e4bc2a0414
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-10-12 15:26:29 +0200

    client comp

--- src/app/(pages)/items/_components/DeleteItems.tsx
+++ src/app/(pages)/items/_components/DeleteItems.tsx
@@ -1,5 +1,6 @@
+"use client";
 import React from "react";
-import ServerFormSubmit from "~/components/common/ServerFormSubmit";
+import Icon from "~/components/common/Icon";
 import { removeCheckedItems } from "~/server/api/items";
 import { type User } from "~/server/auth";
 import type { Item } from "~/server/shared";
@@ -8,23 +9,19 @@ type Props = { items: Item[]; user: User };
 
 const DeleteCheckedItems = ({ items, user }: Props) => {
   const removable = items
-    .filter((item) => item.checked && !item.recipe_ingredient)
+    .filter((item) => item.checked && !item.menuId)
     .map((item) => item.id);
+  const handleRemove = async () =>
+    await removeCheckedItems({
+      ids: removable,
+      user,
+    });
   if (removable.length === 0) return null;
   return (
-    <form
-      className="relative"
-      action={async () => {
-        "use server";
-        await removeCheckedItems({
-          ids: removable,
-          user,
-        });
-      }}
-    >
-      <p className="absolute top-1 left-0.5 text-xs">{removable.length}</p>
-      <ServerFormSubmit icon="Trash" />
-    </form>
+    <div onClick={handleRemove} className="relative">
+      <p className="absolute bottom-1.5 -left-1.5 text-xs">{removable.length}</p>
+      <Icon icon="Trash" />
+    </div>
   );
 };
 
