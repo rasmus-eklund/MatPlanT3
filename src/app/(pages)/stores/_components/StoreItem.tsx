a5b6959c967b5a6329becbf5cecef4e20cc35ee0
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-10-12 17:01:41 +0200

    enable setting default store

--- src/app/(pages)/stores/_components/StoreItem.tsx
+++ src/app/(pages)/stores/_components/StoreItem.tsx
@@ -1,18 +1,37 @@
 import Link from "next/link";
 import RemoveItemDialog from "~/components/common/DeleteModal";
-import { deleteStore } from "~/server/api/stores";
+import Icon from "~/components/common/Icon";
+import ServerFormSubmit from "~/components/common/ServerFormSubmit";
+import { deleteStore, setDefaultStore } from "~/server/api/stores";
 import { type User } from "~/server/auth";
 import type { Stores } from "~/server/shared";
 
 type Props = { store: Stores[number]; deleteable: boolean; user: User };
-const StoreItem = ({ store: { id, name }, deleteable, user }: Props) => {
+const StoreItem = ({
+  store: { id, name, default: isDefault },
+  deleteable,
+  user,
+}: Props) => {
   return (
-    <li className="bg-c2 flex h-10 items-center justify-between rounded-md p-2">
+    <li className="bg-c2 flex h-10 items-center rounded-md p-2">
+      {isDefault ? (
+        <Icon className="text-c3 fill-c3 mx-2.5" icon="Star" />
+      ) : (
+        <form
+          action={async () => {
+            "use server";
+            await setDefaultStore({ id, user });
+          }}
+        >
+          <ServerFormSubmit icon="Star" />
+        </form>
+      )}
       <Link className="text-c5 md:hover:text-c3 text-xl" href={`/stores/${id}`}>
         {name}
       </Link>
       {deleteable && (
         <RemoveItemDialog
+          className="ml-auto"
           icon
           info={{
             name: "din affär",
