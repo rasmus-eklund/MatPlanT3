a8c6a0c6d3b1ceea19c999fd65b5e648421d96c5
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-10-18 07:55:20 +0200

    show user col

--- src/app/(pages)/admin/auditLogs/page.tsx
+++ src/app/(pages)/admin/auditLogs/page.tsx
@@ -1,10 +1,10 @@
 import { WithAuth } from "~/components/common/withAuth";
 import { getAuditLogs } from "~/server/api/auditLog";
-import LogsTable from "./_components/LogsTable";
+import LogsTable from "~/components/common/LogsTable";
 
 const AuditLogs = async () => {
   const logs = await getAuditLogs();
-  return <LogsTable logs={logs} />;
+  return <LogsTable logs={logs} showUser />;
 };
 
 export default WithAuth(AuditLogs, true);
