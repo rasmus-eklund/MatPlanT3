54c69a783979078c3a4d64146fcac7e665608fa8
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2025-10-18 16:19:47 +0200

    add empty state

--- src/app/(pages)/logs/page.tsx
+++ src/app/(pages)/logs/page.tsx
@@ -4,6 +4,13 @@ import LogsTable from "~/components/common/LogsTable";
 
 const AuditLogs = async ({ user }: WithAuthProps) => {
   const logs = await getAuditLogsByUser(user.id);
+  if (!logs.length)
+    return (
+      <div className="flex size-full flex-col items-center justify-center gap-10">
+        <h2 className="text-c1 text-xl">Du har inga loggar ännu</h2>
+        <p>När du använt appen så kommer du att kunna se dina loggar här.</p>
+      </div>
+    );
   return <LogsTable logs={logs} showUser={false} />;
 };
 
