abfb06e62c98a52f8e1d926dcc12de2b25feb04c
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-05-11 08:59:11 +0200

    split type

--- src/stores/admin-ingredient-store.ts
+++ src/stores/admin-ingredient-store.ts
@@ -3,7 +3,7 @@ import type { AllIngredients } from "~/server/shared";
 
 type Ingredient = AllIngredients[number];
 
-export const useAdminIngredientStore = create<{
+type AdminIngredientStore = {
   selectedIng: Ingredient | null;
   selectedCat: Ingredient["category"] | null;
   selectedSub: Ingredient["subcategory"] | null;
@@ -15,28 +15,32 @@ export const useAdminIngredientStore = create<{
   diffCat: boolean;
   diffSub: boolean;
   search: string;
-}>((set, get) => ({
-  diffCat: false,
-  diffSub: false,
-  selectedIng: null,
-  selectedCat: null,
-  selectedSub: null,
-  search: "",
-  setSelectedIng: (ing: Ingredient | null) => set({ selectedIng: ing }),
-  setSelectedCat: (cat: Ingredient["category"] | null) => {
-    set({ diffCat: cat?.id !== get().selectedIng?.category.id });
-    set({ selectedCat: cat });
-  },
-  setSelectedSub: (sub: Ingredient["subcategory"] | null) => {
-    set({ diffSub: sub?.id !== get().selectedIng?.subcategory.id });
-    set({ selectedSub: sub });
-  },
-  setSearch: (search: string) => set({ search }),
-  reset: () =>
-    set({
-      selectedIng: null,
-      selectedCat: null,
-      selectedSub: null,
-      search: "",
-    }),
-}));
+};
+
+export const useAdminIngredientStore = create<AdminIngredientStore>(
+  (set, get) => ({
+    diffCat: false,
+    diffSub: false,
+    selectedIng: null,
+    selectedCat: null,
+    selectedSub: null,
+    search: "",
+    setSelectedIng: (ing: Ingredient | null) => set({ selectedIng: ing }),
+    setSelectedCat: (cat: Ingredient["category"] | null) => {
+      set({ diffCat: cat?.id !== get().selectedIng?.category.id });
+      set({ selectedCat: cat });
+    },
+    setSelectedSub: (sub: Ingredient["subcategory"] | null) => {
+      set({ diffSub: sub?.id !== get().selectedIng?.subcategory.id });
+      set({ selectedSub: sub });
+    },
+    setSearch: (search: string) => set({ search }),
+    reset: () =>
+      set({
+        selectedIng: null,
+        selectedCat: null,
+        selectedSub: null,
+        search: "",
+      }),
+  }),
+);
