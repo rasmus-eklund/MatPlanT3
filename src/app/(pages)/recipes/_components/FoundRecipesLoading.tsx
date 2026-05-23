9ec08b71664dc18c8fddddef6259b954df5ce350
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-05-23 08:30:02 +0200

    fix loading buttons

--- src/app/(pages)/recipes/_components/FoundRecipesLoading.tsx
+++ src/app/(pages)/recipes/_components/FoundRecipesLoading.tsx
@@ -1,7 +1,11 @@
+import Icon from "~/components/common/Icon";
 import { Skeleton } from "~/components/ui/skeleton";
+import { cn } from "~/lib/utils";
+import type { SearchRecipeParams } from "~/types";
 
 type Props = {
   items?: number;
+  params?: Pick<SearchRecipeParams, "limit" | "page">;
 };
 
 export const SearchRecipeLoading = () => {
@@ -19,20 +23,38 @@ export const SearchRecipeLoading = () => {
   );
 };
 
-const FoundRecipesLoading = ({ items = 4 }: Props) => {
+const FoundRecipesLoading = ({ items, params }: Props) => {
+  const page = params?.page ?? 1;
+  const limit = params?.limit ?? 10;
+  const itemCount = items ?? Math.min(limit, 8);
+
   return (
     <section className="bg-c3/80 flex min-h-0 flex-1 flex-col gap-2 rounded-md p-2">
       <div className="flex min-h-0 flex-1 flex-col gap-2 overflow-auto">
-        {Array.from({ length: items }, (_, index) => (
+        {Array.from({ length: itemCount }, (_, index) => (
           <Item key={index} />
         ))}
       </div>
       <div className="flex shrink-0 items-center justify-between gap-2 p-1">
-        <Skeleton className="h-10 w-16" />
-        <Skeleton className="h-6 w-16" />
+        <div className="flex items-center gap-6">
+          <div className="border-input bg-background flex h-10 w-16 items-center justify-between rounded-md border px-3 py-2 text-sm">
+            <span>{limit}</span>
+            <Icon icon="ChevronDown" className="size-4" />
+          </div>
+          <p>Sida: {page}</p>
+        </div>
         <div className="flex items-center gap-2">
-          <Skeleton className="h-10 w-10" />
-          <Skeleton className="h-10 w-10" />
+          <div
+            className={cn(
+              "border-input bg-background flex h-10 w-12.5 items-center justify-center rounded-md border",
+              page === 1 ? "opacity-20" : "",
+            )}
+          >
+            <Icon icon="ChevronLeft" className="size-4" />
+          </div>
+          <div className="border-input bg-background flex h-10 w-12.5 items-center justify-center rounded-md border">
+            <Icon icon="ChevronRight" className="size-4" />
+          </div>
         </div>
       </div>
     </section>
