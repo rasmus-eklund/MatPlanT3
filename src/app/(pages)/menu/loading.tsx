4fe2debc1755c40e6f11f8c2c5aa7a817977117b
Rasmus Eklund <49008491+rasmus-eklund@users.noreply.github.com>
2026-05-24 08:30:40 +0200

    match menu

--- src/app/(pages)/menu/loading.tsx
+++ src/app/(pages)/menu/loading.tsx
@@ -1,28 +1,34 @@
 import { Skeleton } from "~/components/ui/skeleton";
 const Loading = () => {
   return (
-    <div className="flex flex-col gap-2">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+    <div className="flex h-full flex-col">
+      <div className="flex items-center justify-between px-2 py-1 md:px-3">
+        <Skeleton className="h-7 w-16" />
+        <Skeleton className="size-4 rounded-md" />
+      </div>
+      <div className="space-y-2 px-1 md:px-2">
+        <Item />
+        <Item />
+        <Item />
+        <Item />
+      </div>
     </div>
   );
 };
 
 const Item = () => {
   return (
-    <div className="bg-c3/80 flex flex-col gap-2 rounded-md p-2">
-      <Skeleton className="h-8 w-48" />
+    <div className="bg-c2/80 flex flex-col gap-2 rounded-md px-2 font-bold">
+      <Skeleton className="mt-2 h-5 w-48" />
       <div className="flex w-full items-center justify-between gap-2 py-2 select-none">
         <div className="flex items-center gap-2">
-          <Skeleton className="h-6 w-28" />
+          <Skeleton className="h-9 w-28 rounded-md" />
           <div className="flex items-center gap-2">
             <Skeleton className="h-6 w-14" />
             <Skeleton className="size-6" />
           </div>
         </div>
-        <Skeleton className="size-6" />
+        <Skeleton className="size-9 rounded-md" />
       </div>
     </div>
   );
