9b935ca525a100c02319744e365c96082da62542
Rasmus <49008491+rasmus-eklund@users.noreply.github.com>
2026-06-25 23:20:29 +0200

    make client comp

--- src/app/(pages)/stores/_components/StoreItem.tsx
+++ src/app/(pages)/stores/_components/StoreItem.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { toast } from "sonner";
 import RemoveItemDialog from "~/components/common/DeleteModal";
 import Icon from "~/components/common/Icon";
-import ServerFormSubmit from "~/components/common/ServerFormSubmit";
+import { Button } from "~/components/ui/button";
+import { Spinner } from "~/components/ui/spinner";
 import { deleteStore, setDefaultStore } from "~/server/api/stores";
 import type { Stores } from "~/server/shared";
 
@@ -12,18 +17,7 @@ const StoreItem = ({
 }: Props) => {
   return (
     <li className="bg-c2 flex h-10 items-center rounded-md p-2">
-      {isDefault ? (
-        <Icon className="fill-c5 mx-2.5" icon="Star" />
-      ) : (
-        <form
-          action={async () => {
-            "use server";
-            await setDefaultStore({ id });
-          }}
-        >
-          <ServerFormSubmit icon="Star" />
-        </form>
-      )}
+      <Favorite id={id} isDefault={isDefault} />
       <Link className="text-c5 md:hover:text-c3 text-xl" href={`/stores/${id}`}>
         {name}
       </Link>
@@ -36,13 +30,41 @@ const StoreItem = ({
             description:
               "Detta kommer att ta bort din affär och ordningen som du sparat.",
           }}
-          action={async () => {
-            "use server";
-            await deleteStore({ id, name });
-          }}
+          action={() => deleteStore({ id, name })}
         />
       )}
     </li>
+  );
+};
+
+const Favorite = ({ isDefault, id }: { isDefault: boolean; id: string }) => {
+  const [loading, setLoading] = useState(false);
+  const onSetDefaultStore = async () => {
+    setLoading(true);
+    try {
+      await setDefaultStore({ id });
+    } catch (e) {
+      console.error(e);
+      toast.error("Något gick fel...");
+    } finally {
+      setLoading(false);
+    }
+  };
+  if (loading) {
+    return <Spinner className="mx-1" />;
+  }
+  if (isDefault) {
+    return <Icon className="fill-c5 mx-1" icon="Star" />;
+  }
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      disabled={loading}
+      onClick={onSetDefaultStore}
+    >
+      <Icon icon="Star" />
+    </Button>
   );
 };
 
